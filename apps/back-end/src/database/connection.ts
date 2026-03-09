12ca01c224b47123b2b16ffe15683b630ffa08bb
AlexMan123456 <alex.man03888@gmail.com>
2026-03-09 00:32:57 +0000

    Setup initial migration, back-end client, and dev server

---
+++ apps/back-end/src/database/connection.ts
@@ -0,0 +1,38 @@
+import type { NodePgDatabase } from "drizzle-orm/node-postgres";
+
+import { DataError, parseEnv } from "@alextheman/utility";
+// eslint-disable-next-line @alextheman/no-namespace-imports
+import * as schema from "@lexicon/schema";
+import dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+import path from "node:path";
+
+const ENV = parseEnv(process.env.NODE_ENV ?? "development");
+
+const envFilePath = path.resolve(process.cwd(), `.env.${ENV}`);
+
+dotenv.config({
+  path: envFilePath,
+});
+
+const { DATABASE_URL } = process.env;
+if (!DATABASE_URL) {
+  throw new DataError(
+    { envFilePath },
+    "DATABASE_URL_NOT_SET",
+    "Tried to find a database URL in your .env file but could not find it.",
+  );
+}
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+});
+
+const database = drizzle(pool, { schema });
+
+export type Database = NodePgDatabase<typeof schema>;
+
+export { pool };
+export default database;
--- apps/back-end/src/server/routes/users/get.ts
+++ apps/back-end/src/server/routes/users/get.ts
@@ -4,7 +4,7 @@ import useMiddleware from "src/utility/useMiddleware";
 
 function getUsers(router: Router): void {
   router.route("/:userId").get(
-    useMiddleware<{ userId: string }>(async (request, response, next) => {
+    useMiddleware<{ userId: string }>(async (_request, _response, _next) => {
       // Logic here
     }),
   );
