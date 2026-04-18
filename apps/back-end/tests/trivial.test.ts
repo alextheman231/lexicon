9be5576e23ff46a571aa5e7cecae356679ff8ed5
AlexMan123456 <alex.man03888@gmail.com>
2026-04-18 12:25:56 +0100

    Use defineEndpoint for the root API route
    This ensures that we maintain the v1 prefix too.

--- apps/back-end/tests/trivial.test.ts
+++ apps/back-end/tests/trivial.test.ts
@@ -5,7 +5,7 @@ import app from "src/server/app";
 
 describe("Trivial test", () => {
   test("Trivial endpoint query", async () => {
-    const { body } = await request(app).get("/api").expect(200);
+    const { body } = await request(app).get("/api/v1").expect(200);
     expect(body.hello).toBe("world");
   });
 });
