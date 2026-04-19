d20333b4f613c68787607fa22d1b11719e0e68af
AlexMan123456 <alex.man03888@gmail.com>
2026-04-20 00:28:33 +0100

    Add handleFallthroughMiddleware
    This is to be used for the validator middleware functions that runs before the main route logic, and more generally whenever we always want the middleware to call next after running.

---
+++ apps/back-end/src/utility/validators/requireAuth.ts
@@ -0,0 +1,33 @@
+import { DataError } from "@alextheman/utility";
+import { parseUser } from "@lexicon/models";
+
+import { getConnection } from "src/database/connection";
+import { selectUser } from "src/services/users";
+import { selectUserSession } from "src/services/userSessions";
+import handleFallthroughMiddleware from "src/utility/handleFallthroughMiddleware";
+
+const requireAuth = handleFallthroughMiddleware(async (request) => {
+  const connection = getConnection();
+  const sessionId = request.cookies.session;
+  if (!sessionId) {
+    throw new DataError(
+      { sessionId },
+      "AUTH_REQUIRED",
+      "Expected to find a session but none was found.",
+    );
+  }
+
+  const session = await selectUserSession(connection, sessionId);
+
+  if (session === null || session.expiresAt < new Date()) {
+    throw new DataError(
+      { sessionId },
+      "AUTH_REQUIRED",
+      "Expected to find a session but none was found.",
+    );
+  }
+
+  request.user = parseUser(await selectUser(connection, session.userId));
+});
+
+export default requireAuth;
