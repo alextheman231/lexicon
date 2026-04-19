1a4749936c9014fe5d2bae29b058c1f93340d261
AlexMan123456 <alex.man03888@gmail.com>
2026-04-19 20:00:45 +0100

    Allow the Google auth callback to redirect on success

--- apps/back-end/src/server/app.ts
+++ apps/back-end/src/server/app.ts
@@ -1,14 +1,14 @@
-import { stringListToArray } from "@alextheman/utility";
 import cookieParser from "cookie-parser";
 import express from "express";
 
 import { handleErrors } from "src/server/errors";
 import createEndpoints from "src/server/routes";
+import ALLOWED_ORIGINS from "src/utility/constants/ALLOWED_ORIGINS";
 import setupCors from "src/utility/setupCors";
 
 const app = express();
 
-app.use(setupCors(stringListToArray(process.env.ALLOWED_ORIGINS ?? "")));
+app.use(setupCors(ALLOWED_ORIGINS));
 app.use(express.json());
 app.use(cookieParser());
 
--- apps/back-end/src/server/routes/auth.ts
+++ apps/back-end/src/server/routes/auth.ts
@@ -18,6 +18,7 @@ import { getConnection } from "src/database/connection";
 import { insertAuthProvider, selectAuthProvider } from "src/services/auth";
 import { insertUser, selectUser } from "src/services/users";
 import { insertUserSession, selectUserSession } from "src/services/userSessions";
+import ALLOWED_ORIGINS from "src/utility/constants/ALLOWED_ORIGINS";
 import handleMiddleware from "src/utility/handleMiddleware";
 
 const authRouter = Router();
@@ -28,110 +29,148 @@ function getCallbackUrl(originalUrl: string = "/api/v1/auth/google/callback") {
 
 authRouter.get(
   "/google",
-  handleMiddleware(async (_request, response) => {
-    const callbackUrl = getCallbackUrl();
-    const config = await getGoogleConfig();
+  handleMiddleware<ParamsDictionary, unknown, unknown, { redirect: string }>(
+    async (request, response) => {
+      const callbackUrl = getCallbackUrl();
+      const config = await getGoogleConfig();
 
-    const codeVerifier = randomPKCECodeVerifier();
-    const codeChallenge = await calculatePKCECodeChallenge(codeVerifier);
-    const state = randomState();
+      const codeVerifier = randomPKCECodeVerifier();
+      const codeChallenge = await calculatePKCECodeChallenge(codeVerifier);
+      const state = randomState();
 
-    response.cookie("oauth_state", state, {
-      httpOnly: true,
-      sameSite: "lax",
-      secure: ENV === "production",
-    });
-    response.cookie("oauth_pkce_verifier", codeVerifier, {
-      httpOnly: true,
-      sameSite: "lax",
-      secure: ENV === "production",
-    });
+      const { redirect } = request.query;
 
-    const url = buildAuthorizationUrl(config, {
-      redirect_uri: callbackUrl,
-      scope: "openid email profile",
-      code_challenge: codeChallenge,
-      code_challenge_method: "S256",
-      state,
-    });
+      if (typeof redirect !== "string") {
+        throw new DataError({}, "INVALID_REDIRECT", "Missing redirect parameter");
+      }
 
-    response.redirect(url.toString());
-  }),
+      if (!ALLOWED_ORIGINS.includes(redirect)) {
+        throw new DataError(
+          { redirect },
+          "INVALID_REDIRECT",
+          "The provided redirect origin is not allowed by CORS policy.",
+        );
+      }
+
+      response.cookie("oauth_state", state, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: ENV === "production",
+      });
+      response.cookie("oauth_pkce_verifier", codeVerifier, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: ENV === "production",
+      });
+
+      const url = buildAuthorizationUrl(config, {
+        redirect_uri: callbackUrl,
+        scope: "openid email profile",
+        code_challenge: codeChallenge,
+        code_challenge_method: "S256",
+        state,
+      });
+
+      response.cookie("oauth_redirect", redirect, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: ENV === "production",
+      });
+
+      response.redirect(url.toString());
+    },
+  ),
 );
 
 authRouter.get(
   "/google/callback",
-  handleMiddleware<ParamsDictionary, unknown, unknown, { code: string; state: string }>(
-    async (request, response) => {
-      const config = await getGoogleConfig();
+  handleMiddleware<
+    ParamsDictionary,
+    unknown,
+    unknown,
+    { code: string; state: string; redirect: string }
+  >(async (request, response) => {
+    const config = await getGoogleConfig();
 
-      const cookieState = request.cookies.oauth_state;
-      const pkceCodeVerifier = request.cookies.oauth_pkce_verifier;
+    const cookieState = request.cookies.oauth_state;
+    const pkceCodeVerifier = request.cookies.oauth_pkce_verifier;
 
-      if (!pkceCodeVerifier || !cookieState) {
-        throw new DataError({}, "MISSING_OAUTH_DATA", "Missing OAuth cookies");
+    if (!pkceCodeVerifier || !cookieState) {
+      throw new DataError({}, "MISSING_OAUTH_DATA", "Missing OAuth cookies");
+    }
+
+    const callbackUrl = new URL(getCallbackUrl(request.originalUrl));
+
+    if (request.query.state !== cookieState) {
+      // TODO: Create a BaseError class.
+      // I want the error code and error message, however given the sensitivity of the data I do not feel comfortable including it in the error data.
+      // As such, we should not be providing data here, but BaseError has not been created yet so DataError with an empty payload is sufficient for now.
+      throw new DataError({}, "INVALID_STATE", "The state provided is invalid.");
+    }
+
+    const tokens = await authorizationCodeGrant(config, callbackUrl, {
+      pkceCodeVerifier,
+      expectedState: cookieState,
+    });
+
+    response.clearCookie("oauth_state");
+    response.clearCookie("oauth_pkce_verifier");
+
+    const connection = getConnection();
+
+    const { user, session } = await connection.transaction(async (transaction) => {
+      const claims = tokens.claims();
+      if (!claims?.sub || !claims?.email) {
+        throw new DataError({}, "INVALID_GOOGLE_RESPONSE", "Missing required Google claims");
       }
+      const existingProvider = await selectAuthProvider(transaction, claims);
 
-      const callbackUrl = new URL(getCallbackUrl(request.originalUrl));
-
-      if (request.query.state !== cookieState) {
-        // TODO: Create a BaseError class.
-        // I want the error code and error message, however given the sensitivity of the data I do not feel comfortable including it in the error data.
-        // As such, we should not be providing data here, but BaseError has not been created yet so DataError with an empty payload is sufficient for now.
-        throw new DataError({}, "INVALID_STATE", "The state provided is invalid.");
-      }
-
-      const tokens = await authorizationCodeGrant(config, callbackUrl, {
-        pkceCodeVerifier,
-        expectedState: cookieState,
-      });
-
-      response.clearCookie("oauth_state");
-      response.clearCookie("oauth_pkce_verifier");
-
-      const connection = getConnection();
-
-      const { user, session } = await connection.transaction(async (transaction) => {
-        const claims = tokens.claims();
-        if (!claims?.sub || !claims?.email) {
-          throw new DataError({}, "INVALID_GOOGLE_RESPONSE", "Missing required Google claims");
-        }
-        const existingProvider = await selectAuthProvider(transaction, claims);
-
-        if (existingProvider) {
-          const user = parseUser(await selectUser(transaction, existingProvider.userId));
-          const session = await insertUserSession(transaction, { userId: user.id });
-          return { user, session };
-        }
-
-        const [baseUsername] = claims.email.toString().split("@");
-        const username = `${baseUsername}_${randomBytes(3).toString("hex")}`;
-
-        const user = await insertUser(transaction, {
-          email: claims.email.toString(),
-          username,
-          displayName: claims.name?.toString() ?? username,
-        });
-
-        await insertAuthProvider(transaction, {
-          userId: user.id,
-          provider: "google",
-          providerUserId: claims.sub,
-        });
-
+      if (existingProvider) {
+        const user = parseUser(await selectUser(transaction, existingProvider.userId));
         const session = await insertUserSession(transaction, { userId: user.id });
         return { user, session };
+      }
+
+      const [baseUsername] = claims.email.toString().split("@");
+      const username = `${baseUsername}_${randomBytes(3).toString("hex")}`;
+
+      const user = await insertUser(transaction, {
+        email: claims.email.toString(),
+        username,
+        displayName: claims.name?.toString() ?? username,
       });
 
-      response.cookie("session", session.id, {
-        httpOnly: true,
-        sameSite: "lax",
-        secure: ENV === "production",
-        expires: session.expiresAt,
+      await insertAuthProvider(transaction, {
+        userId: user.id,
+        provider: "google",
+        providerUserId: claims.sub,
       });
-      response.status(200).send({ user });
-    },
-  ),
+
+      const session = await insertUserSession(transaction, { userId: user.id });
+      return { user, session };
+    });
+
+    response.cookie("session", session.id, {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: ENV === "production",
+      expires: session.expiresAt,
+    });
+
+    const redirect = request.cookies.oauth_redirect;
+
+    if (typeof redirect !== "string") {
+      throw new DataError({}, "INVALID_REDIRECT", "Missing redirect parameter");
+    }
+
+    response.clearCookie("oauth_redirect");
+
+    if (ENV === "test") {
+      return response.status(200).send({ user });
+    }
+
+    response.redirect(`${redirect}/auth/callback`);
+  }),
 );
 
 authRouter.get(
