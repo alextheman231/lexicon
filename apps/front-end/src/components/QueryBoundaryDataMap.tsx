40033dbac78b4d565cff0e6013d507a4f1300059
AlexMan123456 <alex.man03888@gmail.com>
2026-04-27 23:27:59 +0100

    Add a QueryBoundaryDataMap wrapper that defaults to the provided ID if present and is the correct type
    This makes usage at the call site a little nicer so that we always try and use the ID as the key wherever we can, but if not we fall back to the index or use the user-defined itemKey.

---
+++ apps/front-end/src/components/QueryBoundaryDataMap.tsx
@@ -0,0 +1,30 @@
+import type { QueryBoundaryDataMapProps } from "@alextheman/components";
+
+import { QueryBoundaryDataMap as AlexQueryBoundaryDataMap } from "@alextheman/components";
+
+function QueryBoundaryDataMap<ItemType>({
+  itemKey,
+  children,
+  ...props
+}: QueryBoundaryDataMapProps<ItemType>) {
+  return (
+    <AlexQueryBoundaryDataMap
+      {...props}
+      itemKey={
+        itemKey ??
+        ((item, index) => {
+          return typeof item === "object" &&
+            item !== null &&
+            "id" in item &&
+            (typeof item.id === "string" || typeof item.id === "number")
+            ? item.id
+            : index;
+        })
+      }
+    >
+      {children}
+    </AlexQueryBoundaryDataMap>
+  );
+}
+
+export default QueryBoundaryDataMap;
