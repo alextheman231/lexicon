04e7bf3912babf4c484910f113013a6a2e472baa
AlexMan123456 <alex.man03888@gmail.com>
2026-05-02 22:43:53 +0100

    Build the models package before running the tests
    This ensures we don't run tests against a stale build of the models package after making changes to it.

--- apps/back-end/tests/setup.ts
+++ apps/back-end/tests/setup.ts
@@ -1,8 +1,13 @@
 import { parseBoolean } from "@alextheman/utility";
 import { execa } from "execa";
 
+import path from "node:path";
+
 export async function setup() {
-  if (!process.env.CI && !parseBoolean(process.env.SKIP_RECREATE_DB ?? "false")) {
+  if (!process.env.CI && !parseBoolean(process.env.SKIP_SETUP ?? "false")) {
+    await execa({
+      cwd: path.join(process.cwd(), "..", "..", "packages", "models"),
+    })`pnpm run build`;
     await execa`pnpm run recreate-test-db`;
   }
 }
