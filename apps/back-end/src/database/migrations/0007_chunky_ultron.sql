82e9d6620672764857c833872498b353018e9c6c
AlexMan123456 <alex.man03888@gmail.com>
2026-05-02 16:55:50 +0100

    Remove the CREATED state from the blog state
    It actually isn't helpful - blogs would generally either be initially created as a draft or as a published blog.

---
+++ apps/back-end/src/database/migrations/0007_chunky_ultron.sql
@@ -0,0 +1,17 @@
+
+CREATE TYPE "public"."BLOG_STATE_T_NEW" AS ENUM ('draft', 'published', 'archived');
+
+ALTER TABLE "blogs"
+ALTER COLUMN "state"
+TYPE "public"."BLOG_STATE_T_NEW"
+USING "state"::TEXT::"public"."BLOG_STATE_T_NEW";
+
+ALTER TABLE "blog_state_history"
+ALTER COLUMN "state"
+TYPE "public"."BLOG_STATE_T_NEW"
+USING "state"::TEXT::"public"."BLOG_STATE_T_NEW";
+
+DROP TYPE "public"."BLOG_STATE_T";
+
+ALTER TYPE "public"."BLOG_STATE_T_NEW"
+RENAME TO "BLOG_STATE_T";
