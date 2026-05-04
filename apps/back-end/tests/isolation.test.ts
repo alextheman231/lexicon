2e925c89a3a3c68d1220c1d7cca8649c00965118
AlexMan123456 <alex.man03888@gmail.com>
2026-05-04 22:21:43 +0100

    Add a test to verify tests are independent
    It doesn't pass for now, but it's there in case I want to return to it.

---
+++ apps/back-end/tests/isolation.test.ts
@@ -0,0 +1,56 @@
+import { fillArray } from "@alextheman/utility";
+import { parseBlogSummaries } from "@lexicon/models";
+import { describe, expect, test } from "vitest";
+
+import getTestFixtures from "tests/fixtures";
+
+describe.skip("Isolated test setup", () => {
+  test("Generates 10 blogs", async () => {
+    const { factory, authenticatedClient } = await getTestFixtures();
+
+    const blogs = await fillArray(
+      async () => {
+        return await factory.blogs.insert();
+      },
+      10,
+      { sequential: true },
+    );
+
+    const blogIds = blogs.map((blog) => {
+      return blog.id;
+    });
+
+    const { body } = await authenticatedClient.get("/api/v1/blogs").expect(200);
+
+    const blogSummaries = parseBlogSummaries(body.blogs);
+    expect(blogSummaries.length).toBe(10);
+
+    for (const blogSummary of blogSummaries) {
+      expect(blogIds.includes(blogSummary.id)).toBe(true);
+    }
+  });
+  test("Generates 10 more blogs independently from the above test", async () => {
+    const { factory, authenticatedClient } = await getTestFixtures();
+
+    const blogs = await fillArray(
+      async () => {
+        return await factory.blogs.insert();
+      },
+      10,
+      { sequential: true },
+    );
+
+    const blogIds = blogs.map((blog) => {
+      return blog.id;
+    });
+
+    const { body } = await authenticatedClient.get("/api/v1/blogs").expect(200);
+
+    const blogSummaries = parseBlogSummaries(body.blogs);
+    expect(blogSummaries.length).toBe(10);
+
+    for (const blogSummary of blogSummaries) {
+      expect(blogIds.includes(blogSummary.id)).toBe(true);
+    }
+  });
+});
