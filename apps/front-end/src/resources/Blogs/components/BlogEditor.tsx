86f815da52a3829300a73ccb9cd7c7427a4a47db
AlexMan123456 <alex.man03888@gmail.com>
2026-05-04 01:25:13 +0100

    Add the ability to post a blog

--- apps/front-end/src/resources/Blogs/components/BlogEditor.tsx
+++ apps/front-end/src/resources/Blogs/components/BlogEditor.tsx
@@ -1,4 +1,5 @@
 import type { SerializedEditorState } from "lexical";
+import type { Dispatch, SetStateAction } from "react";
 
 import { DataError } from "@alextheman/utility/v6";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
@@ -6,48 +7,39 @@ import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import { useState } from "react";
 
-import Debug from "src/components/Debug";
 import ContentEditable from "src/resources/Blogs/components/ContentEditable";
 
 interface EditorProps {
   initialContent?: SerializedEditorState;
+  setEditorState: Dispatch<SetStateAction<SerializedEditorState | undefined>>;
 }
 
-function BlogEditor({ initialContent }: EditorProps) {
-  const [editorState, setEditorState] = useState<SerializedEditorState>();
-
+function BlogEditor({ initialContent, setEditorState }: EditorProps) {
   return (
-    <>
-      <LexicalComposer
-        initialConfig={{
-          namespace: "lexicon-editor",
-          theme: {},
-          editorState: (editor) => {
-            if (initialContent) {
-              const editorState = editor?.parseEditorState(initialContent);
-              editor.setEditorState(editorState);
-            }
-          },
-          onError(error: Error) {
-            throw new DataError({ error }, "EDITOR_ERROR", error.message);
-          },
+    <LexicalComposer
+      initialConfig={{
+        namespace: "lexicon-editor",
+        theme: {},
+        editorState: (editor) => {
+          if (initialContent) {
+            const editorState = editor.parseEditorState(initialContent);
+            editor.setEditorState(editorState);
+          }
+        },
+        onError(error: Error) {
+          throw new DataError({ error }, "EDITOR_ERROR", error.message);
+        },
+      }}
+    >
+      <RichTextPlugin contentEditable={<ContentEditable />} ErrorBoundary={LexicalErrorBoundary} />
+      <HistoryPlugin />
+      <OnChangePlugin
+        onChange={(editorState) => {
+          setEditorState(editorState.toJSON());
         }}
-      >
-        <RichTextPlugin
-          contentEditable={<ContentEditable />}
-          ErrorBoundary={LexicalErrorBoundary}
-        />
-        <HistoryPlugin />
-        <OnChangePlugin
-          onChange={(editorState) => {
-            setEditorState(editorState.toJSON());
-          }}
-        />
-      </LexicalComposer>
-      <Debug content={editorState} />
-    </>
+      />
+    </LexicalComposer>
   );
 }
 
