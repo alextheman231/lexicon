e4bb036f65ab339154b05ca1e7735f8182c5bdf9
AlexMan123456 <alex.man03888@gmail.com>
2026-05-04 01:25:13 +0100

    Add the ability to post a blog

--- apps/front-end/src/resources/Blogs/pages/CreateBlog.tsx
+++ apps/front-end/src/resources/Blogs/pages/CreateBlog.tsx
@@ -1,12 +1,95 @@
-import { Page } from "@alextheman/components";
+import type { BlogInsertData } from "@lexicon/models";
+import type { SerializedEditorState } from "lexical";
 
+import { Page, useSnackbar } from "@alextheman/components";
+import { az } from "@alextheman/utility";
+import { BlogState } from "@lexicon/models";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+import { useLocation } from "wouter";
+import z from "zod";
+
+import { useAuth } from "src/AuthContextProvider";
+import useAppForm from "src/hooks/useAppForm";
 import BlogEditor from "src/resources/Blogs/components/BlogEditor";
+import { useCreateBlogMutation } from "src/resources/Blogs/queries";
+import formatError from "src/utility/errors/formatError";
+
+const blogCreationSchema = z.object({
+  title: az.field(z.string()),
+});
 
 function CreateBlog() {
+  const [editorState, setEditorState] = useState<SerializedEditorState | undefined>();
+  const { mutateAsync: uploadBlog, isPending } = useCreateBlogMutation();
+  const { currentUser } = useAuth();
+  const [_, setLocation] = useLocation();
+  const { addSnackbar } = useSnackbar();
+
+  async function onSubmit(data: BlogInsertData) {
+    try {
+      const id = await uploadBlog(data);
+      addSnackbar("Blog created successfully", "success");
+      setLocation(`/blogs/${id}`);
+    } catch (error) {
+      addSnackbar(formatError(error), "error");
+    }
+  }
+
+  const form = useAppForm({
+    defaultValues: {
+      title: "",
+    },
+    onSubmit: async ({ value }) => {
+      if (editorState === undefined) {
+        addSnackbar("Content is required", "error");
+        return;
+      }
+      await onSubmit({
+        ...blogCreationSchema.parse(value),
+        state: BlogState.PUBLISHED,
+        content: editorState,
+      });
+    },
+    validators: {
+      onSubmit: blogCreationSchema,
+    },
+  });
+
   return (
-    <Page title="Create Blog">
-      <BlogEditor />
-    </Page>
+    <form
+      onSubmit={async (event) => {
+        event.preventDefault();
+        await form.handleSubmit();
+      }}
+    >
+      <Page
+        title={
+          <form.AppField name="title">
+            {(field) => {
+              return <field.TextField label="Title" fullWidth />;
+            }}
+          </form.AppField>
+        }
+        disablePadding
+      >
+        <CardContent>
+          <BlogEditor setEditorState={setEditorState} />
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <Stack direction="row" spacing={2}>
+            <form.AppForm>
+              <form.BackButton to={`/users/${currentUser?.id}`} />
+              <form.SubmitButton disabled={editorState === undefined} loading={isPending} />
+            </form.AppForm>
+          </Stack>
+        </CardActions>
+      </Page>
+    </form>
   );
 }
 
--- apps/front-end/src/resources/Blogs/queries.ts
+++ apps/front-end/src/resources/Blogs/queries.ts
@@ -1,7 +1,9 @@
-import type { BlogSummary, BlogView } from "@lexicon/models";
+import type { BlogInsertData, BlogSummary, BlogView } from "@lexicon/models";
 
+import { az } from "@alextheman/utility";
 import { parseBlogSummaries, parseBlogView } from "@lexicon/models";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import z from "zod";
 
 import lexiconAuthenticatedClient from "src/utility/lexiconAuthenticatedClient";
 import queryKeys from "src/utility/queryKeys";
@@ -22,6 +24,20 @@ export function useBlogQuery(blogId: string) {
     queryFn: async () => {
       const { data } = await lexiconAuthenticatedClient.get(`/api/v1/blogs/${blogId}`);
       return parseBlogView(data.blog);
+    },
+  });
+}
+
+export function useCreateBlogMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (blog: BlogInsertData) => {
+      const { data } = await lexiconAuthenticatedClient.post("/api/v1/blogs", blog);
+      return az.with(z.uuid()).parse(data.id);
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: queryKeys.blogs() });
     },
   });
 }
