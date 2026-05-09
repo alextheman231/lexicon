3adb74864e8ef18688aa6652cf9f042fdfc1bcb3
AlexMan123456 <alex.man03888@gmail.com>
2026-05-09 23:52:18 +0100

    Centralise the blog form between the creation page and edit page
    This makes it easier to reuse them across both pages, therefore reducing duplication.

--- apps/front-end/src/resources/Blogs/pages/CreateBlog.tsx
+++ apps/front-end/src/resources/Blogs/pages/CreateBlog.tsx
@@ -1,39 +1,27 @@
-import type { BlogInsertData, User } from "@lexicon/models";
-import type { SerializedEditorState } from "lexical";
+import type { User } from "@lexicon/models";
 
-import { Page, useSnackbar } from "@alextheman/components";
-import { az } from "@alextheman/utility";
+import type { BlogFormSubmitData } from "src/resources/Blogs/components/BlogForm";
+
+import { useSnackbar } from "@alextheman/components";
 import { BlogState } from "@lexicon/models";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Divider from "@mui/material/Divider";
-import Stack from "@mui/material/Stack";
-import { useState } from "react";
 import { useLocation } from "wouter";
-import z from "zod";
 
-import useAppForm from "src/hooks/useAppForm";
-import BlogEditor from "src/resources/Blogs/components/BlogEditor";
+import BlogForm from "src/resources/Blogs/components/BlogForm";
 import { useCreateBlogMutation } from "src/resources/Blogs/queries";
 import formatError from "src/utility/errors/formatError";
-
-const blogCreationSchema = z.object({
-  title: az.field(z.string()),
-});
 
 interface CreateBlogProps {
   currentUser: User;
 }
 
 function CreateBlog({ currentUser }: CreateBlogProps) {
-  const [editorState, setEditorState] = useState<SerializedEditorState | undefined>();
   const { mutateAsync: uploadBlog, isPending } = useCreateBlogMutation();
   const [_, setLocation] = useLocation();
   const { addSnackbar } = useSnackbar();
 
-  async function onSubmit(data: BlogInsertData) {
+  async function onSubmit(data: BlogFormSubmitData) {
     try {
-      const id = await uploadBlog(data);
+      const id = await uploadBlog({ ...data, state: BlogState.PUBLISHED });
       addSnackbar("Blog created successfully", "success");
       setLocation(`/blogs/${id}`);
     } catch (error) {
@@ -41,57 +29,13 @@ function CreateBlog({ currentUser }: CreateBlogProps) {
     }
   }
 
-  const form = useAppForm({
-    defaultValues: {
-      title: "",
-    },
-    onSubmit: async ({ value }) => {
-      if (editorState === undefined) {
-        addSnackbar("Content is required", "error");
-        return;
-      }
-      await onSubmit({
-        ...blogCreationSchema.parse(value),
-        state: BlogState.PUBLISHED,
-        content: editorState,
-      });
-    },
-    validators: {
-      onSubmit: blogCreationSchema,
-    },
-  });
-
   return (
-    <form
-      onSubmit={async (event) => {
-        event.preventDefault();
-        await form.handleSubmit();
-      }}
-    >
-      <Page
-        title={
-          <form.AppField name="title">
-            {(field) => {
-              return <field.TextField label="Title" fullWidth />;
-            }}
-          </form.AppField>
-        }
-        disablePadding
-      >
-        <CardContent>
-          <BlogEditor setEditorState={setEditorState} />
-        </CardContent>
-        <Divider />
-        <CardActions>
-          <Stack direction="row" spacing={2}>
-            <form.AppForm>
-              <form.BackButton to={`/users/${currentUser.id}`} />
-              <form.SubmitButton disabled={editorState === undefined} loading={isPending} />
-            </form.AppForm>
-          </Stack>
-        </CardActions>
-      </Page>
-    </form>
+    <BlogForm
+      defaultValues={{ title: "", content: "" }}
+      onSubmit={onSubmit}
+      back={`/users/${currentUser.id}`}
+      loading={isPending}
+    />
   );
 }
 
