ef8ac1b0dddaca6928ea7a6d71f68568467b104d
AlexMan123456 <alex.man03888@gmail.com>
2026-05-11 20:31:22 +0100

    Revert "Log the API base URL for quick debugging"
    This reverts commit 02c588d6df57ed06f94c3a1f8de4e5374ddd3ccb.

--- apps/front-end/src/App.tsx
+++ apps/front-end/src/App.tsx
@@ -9,9 +9,6 @@ import Router from "src/Router";
 const queryClient = new QueryClient();
 
 function App() {
-  // eslint-disable-next-line no-console
-  console.log(import.meta.env.VITE_API_BASE_URL);
-
   return (
     <ModeProvider>
       <ErrorBoundary FallbackComponent={ErrorPage}>
