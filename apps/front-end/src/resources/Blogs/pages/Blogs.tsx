7a1f43953f4be452da2b208d09582a734318d500
AlexMan123456 <alex.man03888@gmail.com>
2026-05-12 00:22:15 +0100

    Revert "Log the current user for debugging"
    This reverts commit ab78386de4b725dfad1018dac958d598e399280d.

--- apps/front-end/src/resources/Blogs/pages/Blogs.tsx
+++ apps/front-end/src/resources/Blogs/pages/Blogs.tsx
@@ -14,7 +14,6 @@ import TableFooter from "@mui/material/TableFooter";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
-import { useAuth } from "src/AuthContextProvider";
 import PaginationProvider from "src/components/PaginationProvider";
 import QueryBoundaryDataRowsMap from "src/components/QueryBoundaryDataRowsMap";
 import QueryBoundaryProvider from "src/components/QueryBoundaryProvider";
@@ -33,9 +32,6 @@ function Blogs() {
 
   const { data, isPending, error } = useBlogsQuery(pagination.state.paginationSettings);
   const { rows: blogs, totalRecordCount } = data ?? {};
-  const { currentUser } = useAuth();
-  // eslint-disable-next-line no-console
-  console.log(currentUser);
 
   return (
     <PaginationProvider pagination={pagination}>
