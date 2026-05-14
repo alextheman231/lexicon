abf6280a3f871b4bdab1c11157a205a14e145456
AlexMan123456 <alex.man03888@gmail.com>
2026-05-14 01:12:23 +0100

    Use the tsconfigPaths option in the resolve block
    This gets rid of the need to manually specify the src alias in vite.config.ts, as the tsconfig.json already has it so it can use it from there instead.

--- apps/front-end/vite.config.ts
+++ apps/front-end/vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,8 +8,6 @@ export default defineConfig({
   },
   plugins: [react()],
   resolve: {
-    alias: {
-      src: path.resolve(__dirname, "src"),
-    },
+    tsconfigPaths: true,
   },
 });
