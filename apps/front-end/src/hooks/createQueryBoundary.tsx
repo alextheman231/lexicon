1a95aebad3a31918e156a684c078b8b35a309326
AlexMan123456 <alex.man03888@gmail.com>
2026-05-16 16:39:43 +0100

    Refactor the QueryBoundary.Context to not accept isLoading, error, or data
    They are passed into createQueryBoundary now.
    
    Note that the whole errorComponent, errorFunction, codeErrorMap thing still lives on the Lexicon QueryBoundaryProvider for now. I would eventually like to change this so that QueryBoundaryError takes care of that, which should therefore allow the context to only take children instead.

--- apps/front-end/src/hooks/createQueryBoundary.tsx
+++ apps/front-end/src/hooks/createQueryBoundary.tsx
@@ -2,6 +2,9 @@ import type {
   CreateQueryBoundaryParameters,
   DefaultQueryBoundaryComponents,
 } from "@alextheman/components";
+import type { JSX } from "react";
+
+import type { QueryBoundaryProviderProps } from "src/components/QueryBoundaryProvider";
 
 import { createQueryBoundary as createAlexQueryBoundary } from "@alextheman/components";
 
@@ -13,7 +16,9 @@ export interface LexiconQueryBoundaryComponents<DataType> extends Omit<
   DefaultQueryBoundaryComponents<DataType>,
   "Context" | "DataMap"
 > {
-  Context: typeof QueryBoundaryProvider<DataType>;
+  Context: (
+    props: Omit<QueryBoundaryProviderProps<DataType>, "isLoading" | "error" | "data">,
+  ) => JSX.Element;
   DataMap: typeof QueryBoundaryDataMap<DataType>;
   DataRowsMap: typeof QueryBoundaryDataRowsMap<DataType>;
 }
@@ -25,7 +30,33 @@ function createQueryBoundary<DataType>(
 
   return {
     ...QueryBoundary,
-    Context: ({ children, ...props }) => {
+    Context: ({ children, errorComponent, errorFunction, codeErrorMap, ...props }) => {
+      const query = {
+        isLoading: params.query.isLoading,
+        error: params.query.error,
+        data: params.query.data ?? params.query.dataCollection,
+      };
+
+      if (errorComponent) {
+        return (
+          <QueryBoundaryProvider {...query} errorComponent={errorComponent} {...props}>
+            {children}
+          </QueryBoundaryProvider>
+        );
+      }
+
+      if (errorFunction || codeErrorMap) {
+        return (
+          <QueryBoundaryProvider
+            {...query}
+            codeErrorMap={codeErrorMap}
+            errorFunction={errorFunction}
+          >
+            {children}
+          </QueryBoundaryProvider>
+        );
+      }
+
       return (
         <QueryBoundaryProvider
           isLoading={params.query.isLoading}
