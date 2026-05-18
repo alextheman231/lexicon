18e7eb86486e2c79409ce03e9987ba845988bf19
AlexMan123456 <alex.man03888@gmail.com>
2026-05-18 02:26:15 +0100

    Test against mobile in end-to-end

--- apps/end-to-end/playwright.config.ts
+++ apps/end-to-end/playwright.config.ts
@@ -21,26 +21,22 @@ const playwrightConfig: PlaywrightTestConfig = {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
-
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-
-    /* Test against mobile viewports. Not needed now but will be useful when we focus more on mobile testing. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
   ],
   webServer: [
     {
