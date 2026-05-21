54dbc6644f12f596ae600c973ecaf7e4ab3619ae
AlexMan123456 <alex.man03888@gmail.com>
2026-05-21 01:55:00 +0100

    Uninstall the ESLint plugin from the configs package
    I think it can just be a root dependency.

--- packages/configs/eslint.config.js
+++ packages/configs/eslint.config.js
@@ -3,4 +3,10 @@ import alexPlugin from "@alextheman/eslint-plugin";
 export default [
   ...alexPlugin.configs["combined/typescript"],
   ...alexPlugin.configs["internal/package-json"],
+  {
+    rules: {
+      // Can probably be disabled because the peers are installed as dev workspace dependencies so this is a false positive.
+      "package-json/specify-peers-locally": "off",
+    },
+  },
 ];
