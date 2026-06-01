0274c3c88db2da94826560f3799ac0b7e5e984bf
AlexMan123456 <alex.man03888@gmail.com>
2026-06-01 01:02:32 +0100

    Reduce the amount of browsers we test against
    We were previously testing with Chrome, Firefox, Safari, Mobile Chrome, and Mobile Safari, and having that many tests makes CI slow. This selects the most important ones to test (in my opinion at least) and runs tests against those for now.

--- apps/end-to-end/playwright.config.ts
+++ apps/end-to-end/playwright.config.ts
@@ -22,20 +22,12 @@ const playwrightConfig: PlaywrightTestConfig = {
       use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
     {
       name: "Mobile Chrome",
       use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
     },
   ],
   webServer: [
