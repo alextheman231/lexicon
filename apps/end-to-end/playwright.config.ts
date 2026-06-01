9abb35b9894374205e1a3d5e7f4597a2713c7027
AlexMan123456 <alex.man03888@gmail.com>
2026-06-01 01:37:37 +0100

    Allow the playwright-report to be generated locally as well as in CI
    I think it's useful either way. The way it's set up as of now, we display the list no matter what, but we also generate the HTML report but never open it so we can inspect it more in-depth there too.

--- apps/end-to-end/playwright.config.ts
+++ apps/end-to-end/playwright.config.ts
@@ -11,7 +11,7 @@ const playwrightConfig: PlaywrightTestConfig = {
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [["html", { open: "never" }], ["list"]] : "list",
+  reporter: [["html", { open: "never" }], ["list"]],
   use: {
     baseURL: "http://localhost:5173",
     trace: "on-first-retry",
