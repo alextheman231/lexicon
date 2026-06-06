c82439c33eaf91d3c03b4911584f8d7e0bc82b11
AlexMan123456 <alex.man03888@gmail.com>
2026-06-06 15:22:46 +0100

    Skip the setup test at the config level rather than in setup.ts directly

--- apps/end-to-end/tests/setup.ts
+++ apps/end-to-end/tests/setup.ts
@@ -6,10 +6,8 @@ import test from "tests/fixtures";
 
 const REPOSITORY_ROOT = path.join(process.cwd(), "..", "..");
 
-test(process.env.CI ? "Trivial test" : "Reset the database", async () => {
-  if (!process.env.CI) {
-    await execa({
-      cwd: path.join(REPOSITORY_ROOT, "apps", "back-end"),
-    })`pnpm run recreate-end-to-end-db`;
-  }
+test("Reset the database", async () => {
+  await execa({
+    cwd: path.join(REPOSITORY_ROOT, "apps", "back-end"),
+  })`pnpm run recreate-end-to-end-db`;
 });
