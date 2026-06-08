14617d7cfa04a95316019279d68d03857e6a52c7
AlexMan123456 <alex.man03888@gmail.com>
2026-06-09 00:56:00 +0100

    Some more blog revisions tests

--- apps/back-end/tests/server/blogs/getBlogRevisionsByBlogId.test.ts
+++ apps/back-end/tests/server/blogs/getBlogRevisionsByBlogId.test.ts
@@ -1,10 +1,17 @@
+import type { ResourceNotFoundErrorPayload } from "src/utility/errors/resourceNotFoundError";
+
 import { assertNotUndefined, az, fillArray } from "@alextheman/utility";
 import { CodeError, DataError } from "@alextheman/utility/v6";
 import { parseBlogRevisionHistory } from "@lexicon/models";
+import request from "supertest";
 import { describe, expect, test } from "vitest";
 import z from "zod";
 
+import { randomUUID } from "node:crypto";
+
 import getTestFixtures from "tests/fixtures";
+
+import app from "src/server/app";
 
 describe("GET /api/v1/blogs/<blogId>/revisions", () => {
   test("Gets all the blog revisions for the chosen blog", async () => {
@@ -67,5 +74,47 @@ describe("GET /api/v1/blogs/<blogId>/revisions", () => {
     });
 
     expect(error.code).toBe("FORBIDDEN_ACCESS");
+  });
+  test("Returns a 404 error if the blog was not found", async () => {
+    const { authenticatedClient } = await getTestFixtures();
+
+    const missingId = randomUUID();
+
+    const { body } = await authenticatedClient
+      .get(`/api/v1/blogs/${missingId}/revisions`)
+      .expect(404);
+
+    const error = DataError.expectError<ResourceNotFoundErrorPayload>(() => {
+      throw body.error;
+    });
+
+    expect(error.code).toBe("RESOURCE_NOT_FOUND");
+    expect(error.data.resourceId).toBe(missingId);
+    expect(error.data.resourceType).toBe("blog");
+  });
+  test("Does not allow an unauthenticated user to view blog revisions", async () => {
+    const { factory } = await getTestFixtures();
+
+    const { blog } = await factory.blogs.insert();
+
+    const { body } = await request(app).get(`/api/v1/blogs/${blog.id}/revisions`).expect(401);
+
+    const error = CodeError.expectError(() => {
+      throw body.error;
+    });
+
+    expect(error.code).toBe("AUTH_REQUIRED");
+  });
+  test("Errors with 400 on invalid UUID", async () => {
+    const { authenticatedClient } = await getTestFixtures();
+
+    const { body } = await authenticatedClient.get("/api/v1/blogs/invalid/revisions").expect(400);
+
+    const error = DataError.expectError(() => {
+      throw body.error;
+    });
+
+    expect(error.code).toBe("INVALID_UUID");
+    expect(error.data.input).toBe("invalid");
   });
 });
