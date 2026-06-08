31dc0760e34f57f7fd6dd8849a29171bb2748819
AlexMan123456 <alex.man03888@gmail.com>
2026-06-09 00:05:32 +0100

    Only allow the blog author to see their blog revisions
    It is potentially a leak to allow anyone to see all blog revisions of all blogs. As such, we should put a filter on to ensure that only blog authors can see all their revisions. For now, anyway - we may eventually revisit this idea, but I think best to keep revisions private for now.

---
+++ apps/back-end/src/utility/errors/forbiddenAccessError.ts
@@ -0,0 +1,16 @@
+import { APIError } from "@alextheman/utility/v6";
+
+export interface ForbiddenAccessErrorData {
+  userId: string;
+}
+
+function forbiddenAccessError({ userId }: ForbiddenAccessErrorData) {
+  return new APIError(
+    403,
+    "FORBIDDEN_ACCESS",
+    "You do not have permission to access this resource.",
+    { userId },
+  );
+}
+
+export default forbiddenAccessError;
