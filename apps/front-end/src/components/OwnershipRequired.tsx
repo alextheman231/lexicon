5bfc996c567e0155f47b6f3cb94970e908e1c8cd
AlexMan123456 <alex.man03888@gmail.com>
2026-06-12 00:39:37 +0100

    Allow the ownerId to return either an array of ownerIds or a single string
    The array will be useful in cases where we have an array of data and we want to show the unauthorised page if any single data item did not match the owner ID.

--- apps/front-end/src/components/OwnershipRequired.tsx
+++ apps/front-end/src/components/OwnershipRequired.tsx
@@ -3,23 +3,47 @@ import type { AuthRequiredProps } from "src/components/AuthRequired";
 import AuthRequired from "src/components/AuthRequired";
 import UnauthorisedPage from "src/pages/UnauthorisedPage";
 
-interface OwnershipRequiredPropsWithData<
-  DataType extends object = Record<string, unknown>,
-> extends AuthRequiredProps {
+interface OwnershipRequiredPropsWithData<DataType> extends AuthRequiredProps {
   data: DataType;
-  ownerId: (data: DataType) => string;
+  ownerId: (data: DataType) => string | Array<string>;
 }
 
 interface OwnershipRequiredPropsNoData extends AuthRequiredProps {
   data?: never;
-  ownerId: string;
+  ownerId: string | Array<string>;
 }
 
-export type OwnershipRequiredProps<DataType extends object = Record<string, unknown>> =
+export type OwnershipRequiredProps<DataType> =
   | OwnershipRequiredPropsWithData<DataType>
   | OwnershipRequiredPropsNoData;
 
-function OwnershipRequired<DataType extends object = Record<string, unknown>>({
+function resolveOwnerId<DataType>(
+  ownerIds: string | Array<string> | ((data: DataType) => string | Array<string>),
+  data?: DataType,
+): Array<string> | undefined {
+  if (typeof ownerIds === "string") {
+    return [ownerIds];
+  }
+
+  if (Array.isArray(ownerIds)) {
+    return ownerIds;
+  }
+
+  if (typeof ownerIds === "function" && data !== undefined) {
+    const resolvedOwnerIds = ownerIds(data);
+    if (typeof resolvedOwnerIds === "string") {
+      return [resolvedOwnerIds];
+    }
+
+    if (Array.isArray(resolvedOwnerIds)) {
+      return resolvedOwnerIds;
+    }
+  }
+
+  return undefined;
+}
+
+function OwnershipRequired<DataType>({
   ownerId,
   data,
   unauthorisedMessage,
@@ -28,14 +52,17 @@ function OwnershipRequired<DataType extends object = Record<string, unknown>>({
   return (
     <AuthRequired unauthorisedMessage={unauthorisedMessage}>
       {(currentUser) => {
-        const resolvedOwnerId =
-          typeof ownerId === "string" ? ownerId : data ? ownerId(data) : undefined;
+        const resolvedOwnerIds = resolveOwnerId(ownerId, data);
 
-        if (resolvedOwnerId === undefined) {
+        if (resolvedOwnerIds === undefined) {
           return <UnauthorisedPage unauthorisedMessage={unauthorisedMessage} />;
         }
 
-        if (resolvedOwnerId !== currentUser.id) {
+        if (
+          !resolvedOwnerIds.every((id) => {
+            return id === currentUser.id;
+          })
+        ) {
           return <UnauthorisedPage unauthorisedMessage={unauthorisedMessage} />;
         }
 
