48bf7d6972942925755e2674c2e42337a1850ed8
AlexMan123456 <alex.man03888@gmail.com>
2026-06-13 12:51:07 +0100

    Sort revisions in descending order
    That way we get to see the most recent revision first, which makes the most sense to me. The test has also been changed to more strictly assert the ordering.

--- apps/back-end/tests/server/blogs/getBlogRevisionsByBlogId.test.ts
+++ apps/back-end/tests/server/blogs/getBlogRevisionsByBlogId.test.ts
@@ -1,6 +1,6 @@
 import type { ResourceNotFoundErrorPayload } from "src/utility/errors/resourceNotFoundError";
 
-import { assertNotUndefined, fillArray } from "@alextheman/utility";
+import { assertNotUndefined, fillArray, paralleliseArrays } from "@alextheman/utility";
 import { CodeError, DataError } from "@alextheman/utility/v6";
 import { parseBlogRevisionHistory } from "@lexicon/models";
 import request from "supertest";
@@ -17,41 +17,32 @@ describe("GET /api/v1/blogs/<blogId>/revisions", () => {
     const { factory, authenticatedClient, authenticatedUser } = await getTestFixtures();
 
     const blog = await factory.blogs.insert({ author: authenticatedUser });
-    const blogRevisions = await fillArray(
-      async () => {
-        return await factory.blogRevisions.insert({ blog, editor: authenticatedUser });
-      },
-      10,
-      { sequential: true },
-    );
-
-    const factoryRevisionIds = blogRevisions.map((revision) => {
-      return revision.id;
+    const factoryRevisions = (
+      await fillArray(
+        async () => {
+          return await factory.blogRevisions.insert({ blog, editor: authenticatedUser });
+        },
+        10,
+        { sequential: true },
+      )
+    ).toSorted((first, second) => {
+      return second.version - first.version;
     });
 
     const { body } = await authenticatedClient
       .get(`/api/v1/blogs/${blog.id}/revisions`)
       .expect(200);
 
-    const revisions = parseBlogRevisionHistory(body.revisions);
-    expect(revisions.length).toBe(10);
+    const apiRevisions = parseBlogRevisionHistory(body.revisions);
+    expect(apiRevisions.length).toBe(10);
+    expect(factoryRevisions.length).toBe(10);
 
-    for (const revision of revisions) {
-      if (factoryRevisionIds.includes(revision.id)) {
-        const factoryRevision = blogRevisions.find((factoryRevision) => {
-          return factoryRevision.id === revision.id;
-        });
-
-        assertNotUndefined(factoryRevision);
-
-        expect(revision).toMatchObject(factoryRevision);
-      } else {
-        throw new DataError(
-          { factoryRevisionIds, missingId: revision.id },
-          "REVISION_NOT_FOUND",
-          "Could not find the blog revision",
-        );
-      }
+    for (const [apiRevision, factoryRevision] of paralleliseArrays(
+      apiRevisions,
+      factoryRevisions,
+    )) {
+      assertNotUndefined(factoryRevision);
+      expect(apiRevision).toMatchObject(factoryRevision);
     }
   });
   test("Can not get blog revisions for a blog the user does not own", async () => {
