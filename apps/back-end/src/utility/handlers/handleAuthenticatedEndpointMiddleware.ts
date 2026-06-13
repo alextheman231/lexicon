fab1f5b1b226f55ea9d2b91a26059db93c6a68cf
AlexMan123456 <alex.man03888@gmail.com>
2026-06-13 01:44:38 +0100

    Add user request binding to handleEndpointMiddleware
    Previously if the user was signed in, the user wouldn't have been attached to the request even though they should really be. This changes that so that handleEndpointMiddleware now either attaches the user if they exist or gives a null user otherwise. handleAuthenticatedEndpointMiddleware goes one step further to ensure that the user is signed in, throwing an unauthorised error if not.

--- apps/back-end/src/utility/handlers/handleAuthenticatedEndpointMiddleware.ts
+++ apps/back-end/src/utility/handlers/handleAuthenticatedEndpointMiddleware.ts
@@ -2,12 +2,6 @@ import type { User } from "@lexicon/models";
 import type { NextFunction, Request, RequestHandler, Response } from "express";
 import type { ParamsDictionary } from "express-serve-static-core";
 
-import { assertNotNull } from "@alextheman/utility";
-import { parseUser } from "@lexicon/models";
-
-import { getConnection } from "src/database/connection";
-import selectUser from "src/models/users/selectUser";
-import selectUserSession from "src/models/userSessions/selectUserSession";
 import authRequiredError from "src/utility/errors/authRequiredError";
 import handleEndpointMiddleware from "src/utility/handlers/handleEndpointMiddleware";
 
@@ -36,22 +30,9 @@ function handleAuthenticatedEndpointMiddleware<
 ): RequestHandler<ParameterType, ResBody, ReqBody, ReqQuery, Locals> {
   return handleEndpointMiddleware<ParameterType, ResBody, ReqBody, ReqQuery, Locals>(
     async (request, response, next) => {
-      const connection = getConnection();
-      const sessionId = request.cookies.session;
-      if (!sessionId) {
-        throw authRequiredError(sessionId);
+      if (request.user === null) {
+        throw authRequiredError();
       }
-
-      const session = await selectUserSession(connection, sessionId);
-
-      if (session === null || session.expiresAt < new Date()) {
-        throw authRequiredError(sessionId);
-      }
-
-      const currentUser = await selectUser(connection, session);
-      assertNotNull(currentUser);
-      request.user = parseUser(currentUser);
-
       await middleware(
         request as AuthenticatedRequest<ParameterType, ResBody, ReqBody, ReqQuery, Locals>,
         response,
