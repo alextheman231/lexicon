69f67e76e93e168daa26dcd4b7b3ce64d69f56c0
AlexMan123456 <alex.man03888@gmail.com>
2026-06-13 02:13:38 +0100

    Move the current user logic to its own handler separate from the endpoint handlers
    That way each individual endpoint handler doesn't need to also worry about user binding logic - it can now be done once at the app level and the endpoint handlers can be more pure as a result.

--- apps/back-end/src/utility/handlers/handleEndpointMiddleware.ts
+++ apps/back-end/src/utility/handlers/handleEndpointMiddleware.ts
@@ -1,13 +1,6 @@
 import type { RequestHandler } from "express";
 import type { ParamsDictionary } from "express-serve-static-core";
 
-import { az } from "@alextheman/utility";
-import z from "zod";
-
-import { getConnection } from "src/database/connection";
-import selectUser from "src/models/users/selectUser";
-import selectUserSession from "src/models/userSessions/selectUserSession";
-
 function handleEndpointMiddleware<
   ParameterType extends ParamsDictionary,
   ResBody = unknown,
@@ -18,19 +11,6 @@ function handleEndpointMiddleware<
   middleware: RequestHandler<ParameterType, ResBody, ReqBody, ReqQuery, Locals>,
 ): RequestHandler<ParameterType, ResBody, ReqBody, ReqQuery, Locals> {
   return async (request, response, next) => {
-    const connection = getConnection();
-    const sessionId = request.cookies.session;
-    const session = await selectUserSession(connection, sessionId);
-
-    const currentUser = session !== null ? await selectUser(connection, session) : null;
-    request.user =
-      currentUser !== null
-        ? {
-            ...currentUser,
-            dateOfBirth: az.with(z.coerce.date().nullable()).parse(currentUser.dateOfBirth),
-          }
-        : null;
-
     Promise.resolve(middleware(request, response, next)).catch(next);
   };
 }
