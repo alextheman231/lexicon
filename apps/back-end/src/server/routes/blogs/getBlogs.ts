e3dfa85b739ed11dd6490ebd1feb24ba716d4741
AlexMan123456 <alex.man03888@gmail.com>
2026-06-13 22:39:51 +0100

    Restrict filtering blogs by archived/draft state if we're also not filtering by the current user
    This protects us against potential leakage of draft/archived blogs. Furthermore, if no state filter was provided it will now fallback to published, as that is the safest state to filter by.

--- apps/back-end/src/server/routes/blogs/getBlogs.ts
+++ apps/back-end/src/server/routes/blogs/getBlogs.ts
@@ -1,11 +1,12 @@
 import type { Router } from "express";
 
-import { parseBlogFilter } from "@lexicon/models";
+import { BlogState, parseBlogFilter } from "@lexicon/models";
 
 import { getConnection } from "src/database/connection";
 import countBlogs from "src/services/blogs/countBlogs";
 import loadBlogSummaries from "src/services/blogs/loadBlogSummaries";
 import queryBlogIds from "src/services/blogs/queryBlogIds";
+import forbiddenAccessError from "src/utility/errors/forbiddenAccessError";
 import handleEndpointMiddleware from "src/utility/handlers/handleEndpointMiddleware";
 
 function getBlogs(blogs: Router) {
@@ -13,7 +14,18 @@ function getBlogs(blogs: Router) {
     "/",
     handleEndpointMiddleware(async (request, response) => {
       const connection = getConnection();
-      const filters = parseBlogFilter(request.query);
+      const parsedFilters = parseBlogFilter(request.query);
+
+      if (
+        ([BlogState.ARCHIVED, BlogState.DRAFT] as Array<BlogState | undefined>).includes(
+          parsedFilters.state,
+        ) &&
+        parsedFilters.authorId !== request.user?.id
+      ) {
+        throw forbiddenAccessError({ userId: request.user?.id });
+      }
+
+      const filters = { ...parsedFilters, state: parsedFilters.state ?? BlogState.PUBLISHED };
 
       const blogIds = await queryBlogIds(connection, filters);
       const count = await countBlogs(connection, {
