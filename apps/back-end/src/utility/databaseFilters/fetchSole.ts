38903f245531954c2efcec17e31384efa425ed2c
AlexMan123456 <alex.man03888@gmail.com>
2026-06-14 02:05:18 +0100

    Spacing because I'm picky

--- apps/back-end/src/utility/databaseFilters/fetchSole.ts
+++ apps/back-end/src/utility/databaseFilters/fetchSole.ts
@@ -2,6 +2,7 @@ import { DataError } from "@alextheman/utility/v6";
 
 async function fetchSole<ItemType>(query: Promise<Array<ItemType>>): Promise<ItemType | null> {
   const results = await query;
+
   if (results.length > 1) {
     throw new DataError(
       { length: results.length },
@@ -9,6 +10,7 @@ async function fetchSole<ItemType>(query: Promise<Array<ItemType>>): Promise<Ite
       "Expected only one or zero rows to be returned.",
     );
   }
+
   return results[0] ?? null;
 }
 
