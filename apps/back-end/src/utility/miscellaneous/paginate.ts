d18526506ff73bf1a3a4de8d5b8102e6dfa37ae3
AlexMan123456 <alex.man03888@gmail.com>
2026-06-14 23:26:52 +0100

    Refactor buildBlogsQuery to work based on Drizzle's SQL-like syntax
    I think this is better as it means we spend less time trying to fight against appending raw SQL syntax everywhere (e.g. manually appending AND to queries).

--- apps/back-end/src/utility/miscellaneous/paginate.ts
+++ apps/back-end/src/utility/miscellaneous/paginate.ts
@@ -1,11 +1,19 @@
-import type { SQL } from "drizzle-orm";
+import type { Placeholder } from "drizzle-orm";
 
-import { sql } from "drizzle-orm";
+interface PaginationSettings {
+  pageSize: number;
+  pageNumber?: number;
+}
 
-function paginate(pageSize: number, pageNumber: number = 1): SQL {
-  return sql`
-    LIMIT ${pageSize} OFFSET ${(pageNumber - 1) * pageSize}
-  `;
+function paginate<QueryReturn>(
+  query: {
+    limit: (limit: number | Placeholder) => {
+      offset: (offset: number | Placeholder) => QueryReturn;
+    };
+  },
+  { pageSize, pageNumber = 1 }: PaginationSettings,
+) {
+  return query.limit(pageSize).offset((pageNumber - 1) * pageSize);
 }
 
 export default paginate;
