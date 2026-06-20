e164deccbddd73ae3e1afb06324a6c9ee2305abe
AlexMan123456 <alex.man03888@gmail.com>
2026-06-20 02:02:36 +0100

    Refactor query keys
    This provides a more rigourous type for the keys to catch potential misuse of them. It also adds a useQueryInvalidation hook that provides a function that invalidates all nested query keys from the related query keys. I think previously we'd have had a bug because we often just invalidate the entire related query keys array directly, which is not quite the same as invalidating each item in the related query keys array separately.

--- apps/front-end/src/resources/Users/queries/useLogoutMutation.ts
+++ apps/front-end/src/resources/Users/queries/useLogoutMutation.ts
@@ -1,6 +1,7 @@
 import { useQueryClient } from "@tanstack/react-query";
 
 import useMutation from "src/hooks/query/useMutation";
+import useQueryInvalidation from "src/hooks/query/useQueryInvalidation";
 import useLocation from "src/hooks/useLocation";
 import lexiconAuthenticatedClient from "src/utility/lexiconAuthenticatedClient";
 import queryKeys from "src/utility/query/queryKeys";
@@ -8,14 +9,15 @@ import queryKeys from "src/utility/query/queryKeys";
 function useLogoutMutation() {
   const queryClient = useQueryClient();
   const [_, setLocation] = useLocation();
+  const invalidate = useQueryInvalidation("auth");
 
   return useMutation({
     mutationFn: async () => {
       await lexiconAuthenticatedClient.post("/api/v1/auth/logout");
     },
-    onSuccess: () => {
+    onSuccess: async () => {
       queryClient.setQueryData(queryKeys.auth(), null);
-      queryClient.invalidateQueries({ queryKey: queryKeys.auth() });
+      await invalidate();
       setLocation("/");
     },
   });
