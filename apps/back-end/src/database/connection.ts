8914f2092667c0a9bbf89cb6fb6a57d19d355486
AlexMan123456 <alex.man03888@gmail.com>
2026-06-26 00:02:09 +0100

    Remove debug logs

--- apps/back-end/src/database/connection.ts
+++ apps/back-end/src/database/connection.ts
@@ -34,10 +34,6 @@ if (!DATABASE_URL) {
   );
 }
 
-console.info(process.cwd());
-console.info(
-  readFileSync(path.join(process.cwd(), "aws-rds-global-bundle.pem"), "utf8").slice(0, 100),
-);
 const pool = new Pool({
   connectionString: DATABASE_URL,
   ssl:
