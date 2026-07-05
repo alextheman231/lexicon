068fe9fef0518f341bca37a6b76e3414c5389a4c
AlexMan123456 <alex.man03888@gmail.com>
2026-07-05 16:06:43 +0100

    Move the commit SHA display to the user dropdown
    Rather than have it on the banner itself. The commit hash is more of a debugging tool rather than important information everyone needs to know, and so keeping it in the dropdown means that we only need to access it if we really want to.

--- apps/front-end/src/components/PageWrapper/NavigationBottom.tsx
+++ apps/front-end/src/components/PageWrapper/NavigationBottom.tsx
@@ -9,13 +9,10 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
 import { MdHome, MdPerson } from "react-icons/md";
 
 import { useAuth } from "src/AuthContextProvider";
 import UserDropdown from "src/components/UserDropdown";
-import createObjectQueryBoundary from "src/groups/QueryBoundary/creators/createObjectQueryBoundary";
-import useMetadataQuery from "src/queries/useMetadataQuery";
 
 interface NavigationBottomProps {
   children: ReactNode;
@@ -23,9 +20,6 @@ interface NavigationBottomProps {
 
 function NavigationBottom({ children }: NavigationBottomProps) {
   const { currentUser } = useAuth();
-
-  const { data, isPending, error } = useMetadataQuery();
-  const QueryBoundary = createObjectQueryBoundary({ query: { data, isLoading: isPending, error } });
 
   return (
     <Stack spacing={1}>
@@ -38,11 +32,6 @@ function NavigationBottom({ children }: NavigationBottomProps) {
           }
           action={
             <Stack direction="row" spacing={2}>
-              <QueryBoundary.Data>
-                {(metadata) => {
-                  return <Typography>Current SHA: {metadata.commitHash ?? "unknown"}</Typography>;
-                }}
-              </QueryBoundary.Data>
               <ThemeToggle />
               <UserDropdown />
             </Stack>
