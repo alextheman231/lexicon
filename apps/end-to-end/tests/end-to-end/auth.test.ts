3580982356d4f68a64c093b5d75e746d46164190
AlexMan123456 <alex.man03888@gmail.com>
2026-07-05 16:06:43 +0100

    Move the commit SHA display to the user dropdown
    Rather than have it on the banner itself. The commit hash is more of a debugging tool rather than important information everyone needs to know, and so keeping it in the dropdown means that we only need to access it if we really want to.

--- apps/end-to-end/tests/end-to-end/auth.test.ts
+++ apps/end-to-end/tests/end-to-end/auth.test.ts
@@ -3,10 +3,11 @@ import { expect } from "@playwright/test";
 import test from "tests/fixtures";
 
 test.describe("Authentication", () => {
-  test("Can see a sign-in button if not signed in", async ({ page }) => {
+  test("Can see an options button if not signed in", async ({ page }) => {
     await page.goto("/");
-    // The sign-in button is actually a link, as it's declared as a Material UI Button with component={ExternalLink}.
-    await expect(page.getByRole("link", { name: "Sign in" })).toBeVisible();
+    const userDropdownTrigger = page.getByLabel("User options");
+    await expect(userDropdownTrigger).toBeVisible();
+    expect(userDropdownTrigger).toHaveText("Options");
   });
   test("Can see the user dropdown trigger with the user's display name if signed in", async ({
     authenticatedPage,
--- apps/front-end/src/components/UserDropdown.tsx
+++ apps/front-end/src/components/UserDropdown.tsx
@@ -7,106 +7,151 @@ import {
 } from "@alextheman/components/DropdownMenu";
 import { InternalLink } from "@alextheman/components/routing";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import CardContent from "@mui/material/CardContent";
 import Divider from "@mui/material/Divider";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import { useTheme } from "@mui/material/styles";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-import { MdCreate, MdError, MdLogout, MdManageAccounts, MdPerson } from "react-icons/md";
+import { MdCreate, MdError, MdLogin, MdLogout, MdManageAccounts, MdPerson } from "react-icons/md";
 
 import { useAuth } from "src/AuthContextProvider";
-import QueryBoundaryItemWrapper from "src/groups/QueryBoundary/QueryBoundaryItemWrapper";
+import createObjectQueryBoundary from "src/groups/QueryBoundary/creators/createObjectQueryBoundary";
+import useMetadataQuery from "src/queries/useMetadataQuery";
 import formatError from "src/utility/errors/formatError";
 
 function UserDropdown() {
   const { currentUser, currentUserLoading, currentUserError, unauthenticate } = useAuth();
+  const QueryBoundaryUser = createObjectQueryBoundary({
+    query: { data: currentUser, isLoading: currentUserLoading, error: currentUserError },
+  });
+  const { data, isPending, error } = useMetadataQuery();
+  const QueryBoundaryMetadata = createObjectQueryBoundary({
+    query: { data, isLoading: isPending, error },
+  });
   const theme = useTheme();
 
-  return (
-    <QueryBoundaryItemWrapper
-      isLoading={currentUserLoading}
-      data={currentUser}
-      error={currentUserError}
-      errorFallback={(error) => {
-        const errorMessage = formatError(error);
+  const dropdownTrigger = (
+    <DropdownMenuTrigger aria-label="User options">
+      <QueryBoundaryUser.Nullable nullFallback="Options" />
+      <QueryBoundaryUser.Data>
+        {(user) => {
+          return user.displayName ?? user.username;
+        }}
+      </QueryBoundaryUser.Data>
+    </DropdownMenuTrigger>
+  );
 
-        return (
-          <Box sx={{ paddingTop: 0.3 }}>
-            <Tooltip title={errorMessage}>
-              <Box
-                component="span"
-                tabIndex={0}
-                sx={{
-                  display: "inline-flex",
-                  "&:focus-visible": {
-                    outline: "2px solid",
-                    outlineOffset: "2px",
-                  },
-                }}
-              >
-                <MdError
-                  size={30}
-                  color={theme.palette.error.main}
-                  aria-label={errorMessage}
-                  role="img"
-                />
-              </Box>
-            </Tooltip>
-          </Box>
-        );
-      }}
-      nullFallback={
-        <Button
-          component={ExternalLink}
-          href={`/api/v1/auth/google?redirect=${encodeURIComponent(window.location.origin)}`}
-        >
-          Sign in
-        </Button>
-      }
-    >
-      {(user) => {
-        return (
-          <DropdownMenuProvider>
-            <DropdownMenuTrigger aria-label="User options">{user.displayName}</DropdownMenuTrigger>
-            <DropdownMenu>
+  return (
+    <DropdownMenuProvider>
+      <QueryBoundaryUser.Error>
+        {(error) => {
+          const errorMessage = formatError(error);
+          return (
+            <Box sx={{ paddingTop: 0.3 }}>
+              <Tooltip title={errorMessage}>
+                <Box
+                  component="span"
+                  tabIndex={0}
+                  sx={{
+                    display: "inline-flex",
+                    "&:focus-visible": {
+                      outline: "2px solid",
+                      outlineOffset: "2px",
+                    },
+                  }}
+                >
+                  <MdError
+                    size={30}
+                    color={theme.palette.error.main}
+                    aria-label={errorMessage}
+                    role="img"
+                  />
+                </Box>
+              </Tooltip>
+            </Box>
+          );
+        }}
+      </QueryBoundaryUser.Error>
+      <QueryBoundaryUser.Nullable nullFallback={dropdownTrigger} />
+      <QueryBoundaryUser.Data>{dropdownTrigger}</QueryBoundaryUser.Data>
+      <DropdownMenu>
+        <QueryBoundaryUser.Nullable
+          nullFallback={
+            <DropdownMenuItem
+              component={ExternalLink}
+              href={`/api/v1/auth/google?redirect=${encodeURIComponent(window.location.origin)}`}
+            >
+              <ListItemIcon>
+                <MdLogin />
+              </ListItemIcon>
+              <Typography>Sign in</Typography>
+            </DropdownMenuItem>
+          }
+        />
+        <QueryBoundaryUser.Data>
+          {(user) => {
+            return (
               <CardContent>
                 <Typography variant="h6">{user.displayName}</Typography>
                 <Typography variant="subtitle2">{user.username}</Typography>
               </CardContent>
-              <Divider />
-              <DropdownMenuItem component={InternalLink} to="/blogs/new">
-                <ListItemIcon>
-                  <MdCreate />
-                </ListItemIcon>
-                <Typography>Create Blog</Typography>
-              </DropdownMenuItem>
-              <Divider />
-              <DropdownMenuItem component={InternalLink} to={`/users/${user.id}`}>
-                <ListItemIcon>
-                  <MdPerson />
-                </ListItemIcon>
-                <Typography>View Profile</Typography>
-              </DropdownMenuItem>
-              <DropdownMenuItem component={InternalLink} to="/account/edit">
-                <ListItemIcon>
-                  <MdManageAccounts />
-                </ListItemIcon>
-                <Typography>Edit Profile</Typography>
-              </DropdownMenuItem>
-              <Divider />
-              <DropdownMenuItem onClick={unauthenticate}>
-                <ListItemIcon>
-                  <MdLogout />
-                </ListItemIcon>
-                <Typography>Sign out</Typography>
-              </DropdownMenuItem>
-            </DropdownMenu>
-          </DropdownMenuProvider>
-        );
-      }}
-    </QueryBoundaryItemWrapper>
+            );
+          }}
+        </QueryBoundaryUser.Data>
+        <Divider />
+        <QueryBoundaryUser.Data>
+          {(user) => {
+            return (
+              <>
+                <DropdownMenuItem component={InternalLink} to="/blogs/new">
+                  <ListItemIcon>
+                    <MdCreate />
+                  </ListItemIcon>
+                  <Typography>Create Blog</Typography>
+                </DropdownMenuItem>
+                <Divider />
+                <DropdownMenuItem component={InternalLink} to={`/users/${user.id}`}>
+                  <ListItemIcon>
+                    <MdPerson />
+                  </ListItemIcon>
+                  <Typography>View Profile</Typography>
+                </DropdownMenuItem>
+                <DropdownMenuItem component={InternalLink} to="/account/edit">
+                  <ListItemIcon>
+                    <MdManageAccounts />
+                  </ListItemIcon>
+                  <Typography>Edit Profile</Typography>
+                </DropdownMenuItem>
+                <Divider />
+                <DropdownMenuItem onClick={unauthenticate}>
+                  <ListItemIcon>
+                    <MdLogout />
+                  </ListItemIcon>
+                  <Typography>Sign out</Typography>
+                </DropdownMenuItem>
+                <Divider />
+              </>
+            );
+          }}
+        </QueryBoundaryUser.Data>
+        <QueryBoundaryMetadata.Data>
+          {(metadata) => {
+            return (
+              <>
+                <CardContent>
+                  <Typography variant="h6">Metadata</Typography>
+                  <Typography variant="body2">
+                    Current SHA: {metadata.commitHash ?? "unknown"}
+                  </Typography>
+                </CardContent>
+                <Divider />
+              </>
+            );
+          }}
+        </QueryBoundaryMetadata.Data>
+      </DropdownMenu>
+    </DropdownMenuProvider>
   );
 }
 
