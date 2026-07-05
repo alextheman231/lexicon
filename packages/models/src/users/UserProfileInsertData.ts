b7073e41ffd6c627964c3cb1a25c511cbdb5d9f3
AlexMan123456 <alex.man03888@gmail.com>
2026-07-06 00:30:24 +0100

    Allow the description to be nullable
    Otherwise the page crashes if the description is left empty. I have also improved the UI a bit by having the edit page redirect to the actual about tab on success, and showing a toast on success too.

--- packages/models/src/users/UserProfileInsertData.ts
+++ packages/models/src/users/UserProfileInsertData.ts
@@ -4,7 +4,7 @@ import z from "zod";
 export const userProfileInsertSchema = z.object({
   username: z.string().max(100),
   displayName: z.string().max(50).optional(),
-  description: z.string().optional(),
+  description: z.string().optional().nullable(),
 });
 
 export type UserProfileInsertData = z.infer<typeof userProfileInsertSchema>;
