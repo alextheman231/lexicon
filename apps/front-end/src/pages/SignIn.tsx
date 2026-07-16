ea8e8b545141936635155978e0d7934aba78f86e
AlexMan123456 <alex.man03888@gmail.com>
2026-07-16 01:41:24 +0100

    Remove VITE_API_BASE_URL reference
    This is not needed anymore as the front-end builds are served from the back-end, so the back-end and front-end actually reference the same base URL so we don't need to specify the API base URL. I had already changed the UserDropdown itself to not use this anymore but I forgot about this specific button.

--- apps/front-end/src/pages/SignIn.tsx
+++ apps/front-end/src/pages/SignIn.tsx
@@ -8,7 +8,7 @@ function SignIn() {
         sx={{ backgroundColor: "red" }}
         variant="contained"
         component={ExternalLink}
-        href={`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/google?redirect=${encodeURIComponent(window.location.origin)}`}
+        href={`/api/v1/auth/google?redirect=${encodeURIComponent(window.location.origin)}`}
       >
         Sign in with Google
       </Button>
