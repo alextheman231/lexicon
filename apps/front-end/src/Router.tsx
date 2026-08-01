a1152bb2cd6e9cf3df54a6a3fc607096d737b7d4
AlexMan123456 <alex.man03888@gmail.com>
2026-08-02 00:31:11 +0100

    Revert "Add a maintenance banner to notify users about some planned downtime"
    This reverts commit 64c3eb00cd013947c42b3578e3720570db4e12fa.
    
    The infrastructure work is complete.

--- apps/front-end/src/Router.tsx
+++ apps/front-end/src/Router.tsx
@@ -1,6 +1,5 @@
 import { Router as AlexRouter, Switch } from "@alextheman/components/routing";
 import { CodeError } from "@alextheman/utility/v6";
-import Alert from "@mui/material/Alert";
 import { Route } from "wouter";
 
 import BackendError from "src/components/BackendError";
@@ -17,10 +16,6 @@ function Router() {
   return (
     <AlexRouter>
       <PageWrapper>
-        <Alert severity="info" sx={{ marginBottom: 2 }}>
-          Scheduled maintenance: Lexicon will undergo infrastructure maintenance on Saturday 1st
-          August from 10:00pm to 12:00am BST. Some downtime may occur during this period.
-        </Alert>
         <Switch>
           <Route path="/">
             <Blogs />
