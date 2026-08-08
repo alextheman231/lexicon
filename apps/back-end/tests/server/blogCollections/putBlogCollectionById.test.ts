798924537414b9d9e5ad6642ed2b9b44a75d28f2
AlexMan123456 <alex.man03888@gmail.com>
2026-08-08 22:19:05 +0100

    Remove the ability from the PUT blog-collections route to edit items
    I think now it makes more sense for that endpoint to be pure collection metadata editing, whereas the new PUT blogs/:blogId/blog-collections route is what we should use for item assignments. We would never really want to edit collections at the same time as change the item assignments, so this functionality has been removed. That said, I think it still makes sense to want to create an initial collection with items, so this has been maintained.

--- apps/back-end/tests/server/blogCollections/putBlogCollectionById.test.ts
+++ apps/back-end/tests/server/blogCollections/putBlogCollectionById.test.ts
@@ -1,8 +1,7 @@
 import type { EditBlogCollectionData } from "@lexicon/models";
 
-import { assertNotNullable, omitProperties } from "@alextheman/utility";
 import { DataError } from "@alextheman/utility/v6";
-import { parseBlogCollectionItemsResponse, parseBlogCollectionView } from "@lexicon/models";
+import { parseBlogCollectionView } from "@lexicon/models";
 import { describe, expect, test } from "vitest";
 
 import TestFixtures from "tests/fixtures";
@@ -31,49 +30,6 @@ describe("PUT /api/v1/blog-collections/<blogCollectionId>", () => {
     expect(blogCollectionView).toMatchObject(data);
     expect(blogCollectionView.userId).toBe(user.id);
     expect(blogCollectionView.itemCount).toBe(0);
-  });
-  test("Inserts blog collection items if items specified", async () => {
-    const fixtures = new TestFixtures();
-
-    const factory = await fixtures.factory;
-    const user = await fixtures.authenticatedUser;
-    const testClient = await fixtures.authenticatedClient;
-
-    const blogCollection = await factory.blogCollections.insert({ user });
-    const { blog: firstBlog } = await factory.blogs.insertWithRevision();
-    const { blog: secondBlog } = await factory.blogs.insertWithRevision();
-
-    const data: EditBlogCollectionData = {
-      name: "Favourites",
-      description: "My favourite blogs",
-      items: [{ blogId: firstBlog.id }, { blogId: secondBlog.id }],
-    };
-
-    await testClient.put(`/api/v1/blog-collections/${blogCollection.id}`).send(data).expect(200);
-
-    const { body: collectionBody } = await testClient
-      .get(`/api/v1/blog-collections/${blogCollection.id}`)
-      .expect(200);
-    const blogCollectionView = parseBlogCollectionView(collectionBody.blogCollection);
-
-    expect(omitProperties(blogCollectionView, "itemCount")).toMatchObject(
-      omitProperties(data, "items"),
-    );
-    expect(blogCollectionView.userId).toBe(user.id);
-    expect(blogCollectionView.itemCount).toBe(2);
-
-    const { body: itemsBody } = await testClient
-      .get(`/api/v1/blog-collections/${blogCollection.id}/items`)
-      .expect(200);
-    const { items } = parseBlogCollectionItemsResponse(itemsBody);
-
-    assertNotNullable(data.items);
-    const initialItemsBlogIds = data.items.map((item) => {
-      return item.blogId;
-    });
-    for (const item of items) {
-      expect(initialItemsBlogIds).toContain(item.blogId);
-    }
   });
   test("Does not allow users to edit collections that are not theirs", async () => {
     const fixtures = new TestFixtures();
