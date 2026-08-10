47683c0d225b7e9f5b5a0f4abe2ad48544f1ffaa
AlexMan123456 <alex.man03888@gmail.com>
2026-08-10 01:58:17 +0100

    Use the Search component from components to handle the search
    This should remove a fair amount of duplicated logic. Note that I couldn't quite get the Search working with the PaginationGroup as it would keep un-focusing every time we enter something.

--- apps/front-end/src/groups/pagination/index.tsx
+++ apps/front-end/src/groups/pagination/index.tsx
@@ -1,9 +1,12 @@
+import type { SearchProps } from "@alextheman/components";
 import type { JSX } from "react";
 
 import type { ListPaginationProps } from "src/groups/pagination/ListPagination";
 import type { TablePaginationProps } from "src/groups/pagination/TablePagination";
 import type { TableSortLabelProps } from "src/groups/pagination/TableSortLabel";
 import type usePagination from "src/hooks/usePagination";
+
+import { Search } from "@alextheman/components";
 
 import ListPagination from "src/groups/pagination/ListPagination";
 import TablePagination from "src/groups/pagination/TablePagination";
@@ -13,6 +16,7 @@ export interface PaginationComponents<DataType extends object = Record<PropertyK
   TablePagination: (props: Omit<TablePaginationProps<DataType>, "pagination">) => JSX.Element;
   TableSortLabel: (props: Omit<TableSortLabelProps<DataType>, "pagination">) => JSX.Element;
   ListPagination: (props: Omit<ListPaginationProps<DataType>, "pagination">) => JSX.Element;
+  Search: (props: Omit<SearchProps, "rawSearch" | "setRawSearch">) => JSX.Element;
 }
 
 function createPaginationGroup<DataType extends object = Record<PropertyKey, unknown>>(
@@ -27,6 +31,15 @@ function createPaginationGroup<DataType extends object = Record<PropertyKey, unk
     },
     ListPagination: (props) => {
       return <ListPagination pagination={pagination} {...props} />;
+    },
+    Search: (props) => {
+      return (
+        <Search
+          rawSearch={pagination.state.rawSearch}
+          setRawSearch={pagination.actions.setRawSearch}
+          {...props}
+        />
+      );
     },
   };
 }
