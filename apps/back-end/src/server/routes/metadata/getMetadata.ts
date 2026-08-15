7f145768497bad1f7c4eb0b74bb588e3d138aacb
AlexMan123456 <alex.man03888@gmail.com>
2026-08-15 15:13:40 +0100

    Rate limit the metadata endpoint
    loadCurrentUser does still run before it too, so hitting this endpoint isn't necessarily completely free given that it will still need to load the current user first.

--- apps/back-end/src/server/routes/metadata/getMetadata.ts
+++ apps/back-end/src/server/routes/metadata/getMetadata.ts
@@ -1,10 +1,16 @@
 import type { Router } from "express";
 
 import handleEndpointMiddleware from "src/utility/handlers/handleEndpointMiddleware";
+import handleRateLimit from "src/utility/handlers/handleRateLimit";
+import msToSeconds from "src/utility/timeConverters/msToSeconds";
 
 function getMetadata(metadata: Router) {
   metadata.get(
     "/",
+    handleRateLimit({
+      limit: 30,
+      windowMs: msToSeconds(10),
+    }),
     handleEndpointMiddleware((_request, response) => {
       response.status(200).send({
         commitHash: process.env.GITHUB_SHA ?? null,
