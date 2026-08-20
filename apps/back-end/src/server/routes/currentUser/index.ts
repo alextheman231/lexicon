1ec06f2a9605e4ce9ea8c0630822c8be4249a5d6
AlexMan123456 <alex.man03888@gmail.com>
2026-08-20 01:10:11 +0100

    Add initial implementation of PUT /current-user/profile-picture
    The profile picture endpoint is separate to the main /current-user/profile endpoint so that it can be multipart/form-data while the main profile one for now can be JSON. Eventually I would like to add file uploads to the main profile endpoint, but that is something for another time.

--- apps/back-end/src/server/routes/currentUser/index.ts
+++ apps/back-end/src/server/routes/currentUser/index.ts
@@ -2,12 +2,14 @@ import type { Router } from "express";
 
 import getCurrentUser from "src/server/routes/currentUser/getCurrentUser";
 import putCurrentUserProfile from "src/server/routes/currentUser/putCurrentUserProfile";
+import putCurrentUserProfilePicture from "src/server/routes/currentUser/putCurrentUserProfilePicture";
 import registerEndpoints from "src/utility/initialisers/registerEndpoints";
 
 function initialiseCurrentUserRouter(router: Router) {
   registerEndpoints(router, {
     getCurrentUser,
     putCurrentUserProfile,
+    putCurrentUserProfilePicture,
   });
 }
 
