e96f42b70ed1c7f3852e2de51709267a77870412
AlexMan123456 <alex.man03888@gmail.com>
2026-08-27 23:32:36 +0100

    Throw an internal server error if the provided file key parameter is incorrect
    That would suggest the user is trying to access an outdated profile picture.

--- apps/back-end/src/server/routes/users/getUserProfilePictureByUserId.ts
+++ apps/back-end/src/server/routes/users/getUserProfilePictureByUserId.ts
@@ -1,22 +1,35 @@
 import type { Router } from "express";
 
 import { assertNotNull, assertNotUndefined, UUID_REGEX_PATTERN } from "@alextheman/utility";
+import { DataError } from "@alextheman/utility/v6";
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 
 import { getConnection } from "src/database/connection";
 import fileStoreClient from "src/fileStoreClient";
 import selectUser from "src/models/users/selectUser";
+import getIdsFromProfilePictureFileKey from "src/utility/fileKeys/getIdsFromProfilePictureFileKey";
 import handleEndpointMiddleware from "src/utility/handlers/handleEndpointMiddleware";
 
 function getUserProfilePictureByUserId(user: Router) {
   user.get(
     RegExp(`^/(?<userId>${UUID_REGEX_PATTERN})/profile-picture/(?<keyId>${UUID_REGEX_PATTERN})`),
-    handleEndpointMiddleware<{ userId: string }>(async (request, response) => {
+    handleEndpointMiddleware<{ userId: string; keyId: string }>(async (request, response) => {
       const connection = getConnection();
 
       const user = await selectUser(connection, { userId: request.params.userId });
       assertNotNull(user);
       assertNotNull(user.profilePictureFileKey);
+
+      const fileKeyIds = getIdsFromProfilePictureFileKey(user.profilePictureFileKey);
+      assertNotNull(fileKeyIds);
+
+      if (fileKeyIds.keyId !== request.params.keyId) {
+        throw new DataError(
+          { keyId: request.params.keyId },
+          "INCORRECT_FILE_KEY",
+          "The provided file key is incorrect",
+        );
+      }
 
       const file = await fileStoreClient.send(
         new GetObjectCommand({
