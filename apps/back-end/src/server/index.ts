d384d5824eb9fcabe7566657703469ad7656acf4
AlexMan123456 <alex.man03888@gmail.com>
2026-08-29 15:09:39 +0100

    Add the URL to the listening logs
    It makes it a little easier to access.

--- apps/back-end/src/server/index.ts
+++ apps/back-end/src/server/index.ts
@@ -7,7 +7,9 @@ const PORT = parseIntStrict(process.env.PORT ?? "8080");
 app.listen(PORT, (error) => {
   if (error) {
     console.error(error);
+  } else if (process.env.API_BASE_URL) {
+    console.info(`Listening for requests at ${process.env.API_BASE_URL}`);
   } else {
-    console.info(`Listening on port ${PORT}`);
+    console.info(`Listening for requests on port ${PORT}`);
   }
 });
