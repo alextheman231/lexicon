9e44cd557b0434f532c75bd3fb40c862e6275c11
AlexMan123456 <alex.man03888@gmail.com>
2026-08-29 03:08:24 +0100

    Add the logo as the title icon on mobile

--- apps/front-end/src/components/PageWrapper/NavigationBottom.tsx
+++ apps/front-end/src/components/PageWrapper/NavigationBottom.tsx
@@ -1,18 +1,18 @@
 import type { ReactNode } from "react";
 
-import {
-  NavigationBottom as AlexNavigationBottom,
-  InternalLink,
-} from "@alextheman/components/routing";
+import { NavigationBottom as AlexNavigationBottom } from "@alextheman/components/routing";
 import { ThemeToggle } from "@alextheman/components/v8/theme";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
+import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import { MdHome, MdPerson } from "react-icons/md";
 
 import { useAuth } from "src/AuthContextProvider";
 import UserDropdown from "src/components/UserDropdown";
+import useLocation from "src/hooks/useLocation";
+import LexiconLogoClosedBook from "src/icons/LexiconLogoClosedBook";
+import LexiconLogoNoText from "src/icons/LexiconLogoNoText";
 
 interface NavigationBottomProps {
   children: ReactNode;
@@ -20,15 +20,25 @@ interface NavigationBottomProps {
 
 function NavigationBottom({ children }: NavigationBottomProps) {
   const { currentUser } = useAuth();
+  const [location, setLocation] = useLocation();
 
   return (
     <Stack spacing={1}>
       <Card>
         <CardHeader
           title={
-            <Button component={InternalLink} to="/">
-              Lexicon
-            </Button>
+            location === "/" ? (
+              <LexiconLogoNoText style={{ height: 20, width: 20 }} />
+            ) : (
+              // Unfortunately component={InternalLink} doesn't work here so we must do this instead...
+              <IconButton
+                onClick={() => {
+                  setLocation("/");
+                }}
+              >
+                <LexiconLogoClosedBook style={{ height: 20, width: 20 }} />
+              </IconButton>
+            )
           }
           action={
             <Stack direction="row" spacing={2}>
