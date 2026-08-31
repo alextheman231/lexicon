1c39914f8df3defd5ff55784e44287bb2a237a53
AlexMan123456 <alex.man03888@gmail.com>
2026-08-31 18:07:54 +0100

    Allow blogs to be archived from the front-end

---
+++ apps/front-end/src/resources/Blogs/components/ArchiveBlogDropdownMenuItem.tsx
@@ -0,0 +1,62 @@
+import type { DropdownMenuItemProps } from "@alextheman/components/DropdownMenu";
+import type { BlogView } from "@lexicon/models";
+
+import { DropdownMenuItem, useDropdownMenuContext } from "@alextheman/components/DropdownMenu";
+import { useSnackbarContext } from "@alextheman/components/snackbar";
+import { BlogState } from "@lexicon/models";
+import { useState } from "react";
+
+import ArchiveBlogDialog from "src/resources/Blogs/components/ArchiveBlogDialog";
+import useEditBlogMutation from "src/resources/Blogs/queries/useEditBlogMutation";
+import formatError from "src/utility/errors/formatError";
+
+interface ArchiveBlogDropdownMenuItemProps extends Omit<DropdownMenuItemProps, "onClick"> {
+  blogId: string;
+}
+
+function ArchiveBlogDropdownMenuItem({ blogId, ...props }: ArchiveBlogDropdownMenuItemProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const { mutateAsync: updateBlog, isPending } = useEditBlogMutation(blogId);
+  const { addSnackbar } = useSnackbarContext();
+  const { closeMenu } = useDropdownMenuContext();
+
+  async function handleArchive(blog: BlogView) {
+    try {
+      await updateBlog({
+        title: blog.title,
+        content: blog.content,
+        state: BlogState.ARCHIVED,
+      });
+      addSnackbar("Blog archived", { severity: "success" });
+      closeMenu();
+    } catch (error) {
+      addSnackbar(formatError(error), { severity: "error" });
+    }
+  }
+
+  return (
+    <>
+      <DropdownMenuItem
+        onClick={(event) => {
+          event.preventDefault();
+          setIsDialogOpen(true);
+        }}
+        {...props}
+      >
+        Archive Blog
+      </DropdownMenuItem>
+      <ArchiveBlogDialog
+        open={isDialogOpen}
+        onClose={() => {
+          setIsDialogOpen(false);
+          closeMenu();
+        }}
+        onSubmit={handleArchive}
+        blogId={blogId}
+        disabled={isPending}
+      />
+    </>
+  );
+}
+
+export default ArchiveBlogDropdownMenuItem;
