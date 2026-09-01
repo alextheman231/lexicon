13f215c6cd148ef4d5c3a8b601d240428340600e
AlexTheMan <alex.man03888@gmail.com>
2026-09-01 09:11:00 +0100

    Manually fix formatting errors like I am back in the stone age
    I am editing the code from GitHub on my phone and therefore do not have terminal access...

--- apps/front-end/src/resources/Blogs/components/ArchiveBlogDialog.tsx
+++ apps/front-end/src/resources/Blogs/components/ArchiveBlogDialog.tsx
@@ -48,13 +48,13 @@ function ArchiveBlogDialog({
             return (
               <>
                 <Typography>
-                  Are you sure you want to archive "{blog.title}"?
+                 Are you sure you want to archive "{blog.title}"?
                 </Typography>
                 <Typography>
                   It will be archived for 30 days, after which it will be properly deleted.
                 </Typography>
               </>
-             )
+            )
           }}
         </QueryBoundaryBlog.Data>
       </DialogContent>
