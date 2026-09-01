fa3eda6436c98180666d5f2c39a1e5f37e3c3ccf
AlexMan123456 <alex.man03888@gmail.com>
2026-09-01 01:05:34 +0100

    Do not allow adding archived blogs to blog collections
    They should be treated as nonexistent for the most part and so should not be part of blog collections.

--- apps/back-end/src/services/blogCollections/mutations/setBlogCollectionsForBlog.ts
+++ apps/back-end/src/services/blogCollections/mutations/setBlogCollectionsForBlog.ts
@@ -1,5 +1,9 @@
 import type { Connection } from "src/database/connection";
 
+import { DataError } from "@alextheman/utility/v6";
+import { BlogState } from "@lexicon/models";
+
+import selectBlog from "src/models/blogs/selectBlog";
 import createBlogCollectionItems from "src/services/blogCollections/mutations/createBlogCollectionItems";
 import removeBlogCollectionItem from "src/services/blogCollections/mutations/removeBlogCollectionItem";
 import queryBlogCollectionIds from "src/services/blogCollections/views/queryBlogCollectionIds";
@@ -9,6 +13,20 @@ async function setBlogCollectionsForBlog(
   blogId: string,
   desiredCollectionIds: Array<string>,
 ): Promise<void> {
+  const blog = await selectBlog(connection, blogId);
+
+  if (blog === null) {
+    return;
+  }
+
+  if (blog.state === BlogState.ARCHIVED) {
+    throw new DataError(
+      { resourceType: "blog", resourceId: blog.id },
+      "RESOURCE_NOT_FOUND",
+      "Could not find blog in database.",
+    );
+  }
+
   const currentCollectionIds = await queryBlogCollectionIds(connection, { blogId });
 
   const collectionsToAddTo: Array<string> = [];
