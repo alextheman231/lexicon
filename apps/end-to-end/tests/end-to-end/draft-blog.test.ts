eb67c984a035fafbefc5973e8d68210e39b67014
AlexMan123456 <alex.man03888@gmail.com>
2026-09-05 23:44:35 +0100

    Simplify the draft-blog test
    It is getting very flaky in CI. As such, the content of it has been made simpler which I am hoping will make it a little less flaky.

--- apps/end-to-end/tests/end-to-end/draft-blog.test.ts
+++ apps/end-to-end/tests/end-to-end/draft-blog.test.ts
@@ -1,4 +1,4 @@
-import { normaliseIndents, UUID_REGEX_PATTERN } from "@alextheman/utility";
+import { UUID_REGEX_PATTERN } from "@alextheman/utility";
 import { expect } from "@playwright/test";
 
 import test from "tests/fixtures";
@@ -6,63 +6,10 @@ import test from "tests/fixtures";
 test.describe("Draft blog", () => {
   test("Can save a blog to drafts and then publish it", async ({ authenticatedPage, baseURL }) => {
     await authenticatedPage.goto("/blogs/new");
-    await authenticatedPage.getByLabel("Title").fill("HCP Terraform Plan Output");
+    await authenticatedPage.getByLabel("Title").fill("My Lexicon Blog");
     const editor = authenticatedPage.locator('[data-lexical-editor="true"]');
 
-    const content = normaliseIndents`
-            Terraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:
-            - destroy
-
-            Terraform will perform the following actions:
-
-            # module.services.module.lexicon_database.neon_project.default will be destroyed
-            - resource "neon_project" "default" {
-                - allowed_ips               = [] -> null
-                - compute_provisioner       = "k8s-neonvm" -> null
-                - connection_uri            = (sensitive value) -> null
-                - connection_uri_pooler     = (sensitive value) -> null
-                - database_host             = "ep-polished-night-akgrjxe2.c-3.us-west-2.aws.neon.tech" -> null
-                - database_host_pooler      = "ep-polished-night-akgrjxe2-pooler.c-3.us-west-2.aws.neon.tech" -> null
-                - database_name             = "lexicon-prod" -> null
-                - database_password         = (sensitive value) -> null
-                - database_user             = "lexicon-prod_owner" -> null
-                - default_branch_id         = "br-cool-grass-akhc5ohk" -> null
-                - default_endpoint_id       = "ep-polished-night-akgrjxe2" -> null
-                - history_retention_seconds = 21600 -> null
-                - id                        = "withered-mouse-11680714" -> null
-                - name                      = "Lexicon" -> null
-                - org_id                    = "org-calm-sky-47179957" -> null
-                - pg_version                = 18 -> null
-                - region_id                 = "aws-us-west-2" -> null
-                - store_password            = "yes" -> null
-
-                - branch {
-                    - database_name = "lexicon-prod" -> null
-                    - id            = "br-cool-grass-akhc5ohk" -> null
-                    - name          = "main" -> null
-                    - role_name     = "lexicon-prod_owner" -> null
-                    }
-
-                - default_endpoint_settings {
-                    - autoscaling_limit_max_cu = 0.25 -> null
-                    - autoscaling_limit_min_cu = 0.25 -> null
-                    - id                       = "ep-polished-night-akgrjxe2" -> null
-                    - suspend_timeout_seconds  = 0 -> null
-                    }
-
-                - maintenance_window {
-                    - end_time   = "09:00" -> null
-                    - start_time = "08:00" -> null
-                    - weekdays   = [
-                        - 5,
-                        ] -> null
-                    }
-
-                - quota {}
-                }
-
-            Plan: 0 to add, 0 to change, 1 to destroy.
-        `;
+    const content = "This is my Lexicon blok";
 
     await editor.click();
     await authenticatedPage.keyboard.insertText(content);
@@ -91,67 +38,13 @@ test.describe("Draft blog", () => {
     await editItem.click();
     await authenticatedPage.waitForURL(RegExp(`^${baseURL}/blogs/${UUID_REGEX_PATTERN}/edit$`));
 
-    const editedContent = normaliseIndents`
-            Terraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:
-                + add
-
-            Terraform will perform the following actions:
-
-            # module.services.module.lexicon_database.neon_project.default will be created
-            + resource "neon_project" "default" {
-                + allowed_ips               = []
-                + compute_provisioner       = "k8s-neonvm"
-                + connection_uri            = (sensitive value) 
-                + connection_uri_pooler     = (sensitive value) 
-                + database_host             = "ep-polished-night-akgrjxe2.c-3.us-west-2.aws.neon.tech" 
-                + database_host_pooler      = "ep-polished-night-akgrjxe2-pooler.c-3.us-west-2.aws.neon.tech" 
-                + database_name             = "lexicon-prod" 
-                + database_password         = (sensitive value) 
-                + database_user             = "lexicon-prod_owner" 
-                + default_branch_id         = "br-cool-grass-akhc5ohk" 
-                + default_endpoint_id       = "ep-polished-night-akgrjxe2" 
-                + history_retention_seconds = 21600 
-                + id                        = "withered-mouse-11680714" 
-                + name                      = "Lexicon" 
-                + org_id                    = "org-calm-sky-47179957" 
-                + pg_version                = 18 
-                + region_id                 = "aws-us-west-2" 
-                + store_password            = "yes" 
-
-                + branch {
-                    + database_name = "lexicon-prod" 
-                    + id            = "br-cool-grass-akhc5ohk" 
-                    + name          = "main" 
-                    + role_name     = "lexicon-prod_owner" 
-                    }
-
-                + default_endpoint_settings {
-                    + autoscaling_limit_max_cu = 0.25 
-                    + autoscaling_limit_min_cu = 0.25 
-                    + id                       = "ep-polished-night-akgrjxe2" 
-                    + suspend_timeout_seconds  = 0 
-                    }
-
-                + maintenance_window {
-                    + end_time   = "09:00" 
-                    + start_time = "08:00" 
-                    + weekdays   = [
-                        + 5,
-                        ] 
-                    }
-
-                + quota {}
-                }
-
-            Plan: 1 to add, 0 to change, 0 to destroy.
-        `;
+    const editedContent = "This is my Lexicon blog.";
 
     const editPageEditor = authenticatedPage.locator('[data-lexical-editor="true"]');
 
     await editPageEditor.click();
-    await authenticatedPage.keyboard.press("ControlOrMeta+A");
     await authenticatedPage.keyboard.press("Backspace");
-    await editPageEditor.fill(editedContent);
+    await authenticatedPage.keyboard.type("g.");
 
     const submitButton = authenticatedPage.getByRole("button", { name: "Submit" });
     await expect(submitButton).toBeEnabled();
@@ -167,7 +60,7 @@ test.describe("Draft blog", () => {
       authenticatedPage.waitForURL(RegExp(`^${baseURL}/blogs/${UUID_REGEX_PATTERN}$`)),
     ]);
 
-    const newTitle = authenticatedPage.getByText("HCP Terraform Plan Output").first();
+    const newTitle = authenticatedPage.getByText("My Lexicon Blog").first();
     await expect(newTitle).toBeVisible();
     await expect(authenticatedPage.getByText(editedContent)).toBeVisible();
 
