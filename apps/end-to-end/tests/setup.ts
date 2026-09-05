e5fce75e0bd4b070a6cd1435f3d3287ab7299f2b
AlexMan123456 <alex.man03888@gmail.com>
2026-09-06 00:03:18 +0100

    Change the setup test to just be a regular Node script
    Otherwise locally when running pnpm test, it will try to build the back-end after it had already started the dev server, therefore leaving us with a stale version. This fixes that by moving the logic out of its own test file, instead being just a regular TypeScript script that always does this before even running Playwright. If we are in CI this script should be skipped (as the end-to-end CI workflow does its own setup already).

--- apps/end-to-end/tests/setup.ts
+++ apps/end-to-end/tests/setup.ts
@@ -2,16 +2,16 @@ import { execa } from "execa";
 
 import path from "node:path";
 
-import test from "tests/fixtures";
+if (!process.env.CI) {
+  const REPOSITORY_ROOT = path.join(process.cwd(), "..", "..");
+  const BACK_END_ROOT = path.join(REPOSITORY_ROOT, "apps", "back-end");
 
-const REPOSITORY_ROOT = path.join(process.cwd(), "..", "..");
-const BACK_END_ROOT = path.join(REPOSITORY_ROOT, "apps", "back-end");
-
-test("Setup", async () => {
   await execa({
     cwd: REPOSITORY_ROOT,
+    stdio: "inherit",
   })`pnpm run build-static`;
   await execa({
     cwd: BACK_END_ROOT,
+    stdio: "inherit",
   })`pnpm run recreate-end-to-end-db`;
-});
+}
