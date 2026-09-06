8776b04d66d8fac58af03ba036811635abf6e3b4
AlexMan123456 <alex.man03888@gmail.com>
2026-09-06 17:50:03 +0100

    Revert "Log the VITE_SENTRY_DSN temporarily"
    This reverts commit 730ea49c81d36f87bb042b8750f6e2492a4702a8.
    
    I've finished debugging the issue and hope the above commit fixes it.

--- apps/front-end/src/main.tsx
+++ apps/front-end/src/main.tsx
@@ -8,8 +8,6 @@ import { createRoot } from "react-dom/client";
 import App from "src/App";
 import ErrorPage from "src/pages/ErrorPage";
 
-// eslint-disable-next-line no-console -- Temporary console.log so I can see if VITE_SENTRY_DSN is actually being used in production
-console.log(import.meta.env.VITE_SENTRY_DSN);
 if (import.meta.env.PROD && import.meta.env.VITE_SENTRY_DSN) {
   init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
