207ccd102a295131123048d51d989aafe7ed5b4a
AlexMan123456 <alex.man03888@gmail.com>
2026-09-12 01:08:16 +0100

    Remove irrelevant linting rules
    Transactions are no longer enforced by ESLint - instead we only type services that require a transaction as needing a transaction.
    
    setConnection is also no longer a thing as we mock the connection in tests instead.

--- packages/configs/src/eslint/backend/index.ts
+++ packages/configs/src/eslint/backend/index.ts
@@ -1,3 +1,2 @@
-export { default as backendEndpoints } from "src/eslint/backend/endpoints";
 export { default as backendMain } from "src/eslint/backend/main";
 export { default as backendSchema } from "src/eslint/backend/schema";
