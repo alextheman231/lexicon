882b7ca5fda57e4ed53c03e07e0e74f7e5eff9b8
AlexMan123456 <alex.man03888@gmail.com>
2026-09-12 01:08:16 +0100

    Remove irrelevant linting rules
    Transactions are no longer enforced by ESLint - instead we only type services that require a transaction as needing a transaction.
    
    setConnection is also no longer a thing as we mock the connection in tests instead.

--- packages/configs/src/eslint/backend/main.ts
+++ packages/configs/src/eslint/backend/main.ts
@@ -4,16 +4,8 @@ import alexPlugin from "@alextheman/eslint-plugin";
 import { sortObjects } from "@alextheman/eslint-plugin/internal";
 import perfectionist from "eslint-plugin-perfectionist";
 
-import baseRestrictedImports from "src/eslint/backend/restrictedImports/baseRestrictedImports";
-
 const backendMain: Array<Linter.Config> = [
   ...alexPlugin.configs["combined/typescript"],
-  {
-    files: ["**/*.ts"],
-    rules: {
-      "no-restricted-imports": ["error", baseRestrictedImports],
-    },
-  },
   {
     files: ["**/index.ts"],
     plugins: {
